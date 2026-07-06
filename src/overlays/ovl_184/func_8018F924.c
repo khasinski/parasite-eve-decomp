ce13b8a667e68487f0f6cb6daa553d07044804f9
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 13:44:15 +0200

    Decompile ovl_184 field initializer

---
+++ src/overlays/ovl_184/func_8018F924.c
@@ -0,0 +1,7 @@
+void func_8018F924(void *arg0, void *arg1, char *arg2) {
+    *(short *)(arg2 + 0x10) = 0x100;
+    *(short *)(arg2 + 8) = 0;
+    *(short *)(arg2 + 0xA) = 0;
+    *(short *)(arg2 + 0xC) = 0;
+    *(short *)(arg2 + 0x12) = 1;
+}
