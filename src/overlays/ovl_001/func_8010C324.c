b66496b40fe196498dc1bb43b2d829ca8ade5146
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-06 14:33:39 +0200

    Decompile small ovl_001 helper functions

---
+++ src/overlays/ovl_001/func_8010C324.c
@@ -0,0 +1,5 @@
+void func_8010C27C(void);
+
+void func_8010C324(void) {
+    func_8010C27C();
+}
---
+++ src/overlays/ovl_001/func_8010C3BC.c
@@ -0,0 +1,5 @@
+void func_80073CF4(int mode, void *value);
+
+void func_8010C3BC(void *value) {
+    func_80073CF4(0, value);
+}
