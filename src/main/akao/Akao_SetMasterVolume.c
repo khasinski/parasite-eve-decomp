cd9413f50faed16060c40bfb3f347b9fc01e7af6
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-07 21:07:40 +0200

    Replace O1 flag in master volume

--- src/main/akao/Akao_SetMasterVolume.c
+++ src/main/akao/Akao_SetMasterVolume.c
@@ -1,11 +1,12 @@
-/* CC1_FLAGS: -O1 */
-
 extern unsigned char *_spu_RXX;
 extern short g_SpuMasterVolume[];
 
 void Akao_SetMasterVolume(short left, short right) {
+    short *vol;
+
     *(short *)(_spu_RXX + 0x184) = left;
     *(short *)(_spu_RXX + 0x186) = right;
-    g_SpuMasterVolume[0] = left;
-    g_SpuMasterVolume[1] = right;
+    asm volatile("" : "=r"(vol) : "0"(g_SpuMasterVolume));
+    vol[0] = left;
+    vol[1] = right;
 }
