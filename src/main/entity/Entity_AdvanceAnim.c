43a2645857efb9bb4acd55e1e8d3433275e26954
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-07 19:29:57 +0200

    Remove redundant O3 flags

--- src/main/entity/Entity_AdvanceAnim.c
+++ src/main/entity/Entity_AdvanceAnim.c
@@ -1,4 +1,4 @@
-/* CC1_FLAGS: -G0 -g3 -O3 */
+/* CC1_FLAGS: -G0 -g3 */
 /* MASPSX_FLAGS: -G0 --use-comm-section --expand-div */
 
 typedef unsigned char u8;
--- src/main/task/Task_PlayFieldSfx.c
+++ src/main/task/Task_PlayFieldSfx.c
@@ -1,4 +1,4 @@
-/* CC1_FLAGS: -G8 -g3 -O3 */
+/* CC1_FLAGS: -G8 -g3 */
 /* MASPSX_FLAGS: -G8 --use-comm-section */
 
 extern char *g_CurrentEntity[];
