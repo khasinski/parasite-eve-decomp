9435d58111c3de3fcac3fb0b0c65b8284b07e080
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-07 19:21:35 +0200

    Remove unused Render_InitSceneGeom flags

--- src/main/main/Render_InitSceneGeom.c
+++ src/main/main/Render_InitSceneGeom.c
@@ -1,5 +1,5 @@
-/* CC1_FLAGS: -G8 --framepointer --diagnostics-color=always */
-/* MASPSX_FLAGS: -G8 --framepointer --expand-div --macro-inc --dont-force-G0 --aspsx-version=2.86 */
+/* CC1_FLAGS: -G8 */
+/* MASPSX_FLAGS: -G8 --expand-div */
 
 #include "include_asm.h"
 INCLUDE_ASM("asm/USA/main/nonmatchings/main/Render_InitSceneGeom", Render_InitSceneGeom);
