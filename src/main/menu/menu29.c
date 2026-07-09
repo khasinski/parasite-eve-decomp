adee351359c4ee3f7a4aee17ad51aa92769236d2
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-09 15:48:50 +0200

    Decompile battle undo and DS searchdir

--- src/main/menu/menu29.c
+++ src/main/menu/menu29.c
@@ -22,7 +22,17 @@ extern BattleCmdEntry g_BattleCmdStack[];
 extern BattleCmdEntry *D_8009D014;
 extern char D_800A1B30[];
 
-void BattleCmd_UndoPending(void);
+typedef signed char s8;
+typedef short s16;
+typedef unsigned int u32;
+
+extern u32 D_800A1AA0[];
+extern s16 D_800C0E48[];
+extern s8 D_800C0E20[];
+extern s8 D_800C0E22[];
+
+int Inv_CheckSlotUsable(int data);
+int Inv_FindIndexByData(int data);
 
 int g_MenuCommandResult;
 
@@ -87,7 +97,57 @@ BattleCmdEntry *BattleCmd_AllocSlot(void) {
     return top;
 }
 
-INCLUDE_ASM("asm/USA/main/nonmatchings/menu/menu29", BattleCmd_UndoPending);
+void BattleCmd_UndoPending(void) {
+    register u32 *entry asm("$4");
+    register u32 *entry_tmp asm("$2");
+    register u32 *top asm("$3");
+    int opcode;
+    s16 *slot;
+    int data;
+    int index;
+
+    top = D_8009D014;
+    if (D_800A1AA0 < top) {
+        entry_tmp = top - 9;
+        asm volatile("" : "=r"(entry_tmp) : "0"(entry_tmp));
+        opcode = top[-9];
+        asm volatile("addu %0,%1,$0" : "=r"(entry) : "r"(entry_tmp));
+        asm volatile("sw $2,%gp_rel(D_8009D014)($gp)");
+
+        switch (opcode) {
+        case 0:
+            slot = &D_800C0E48[entry[2]];
+            if (*slot == 0) {
+                *slot = entry[1];
+            } else {
+                Inv_CheckSlotUsable(entry[1]);
+            }
+            break;
+
+        case 1:
+            break;
+
+        case 2:
+            D_800C0E20[0] = Inv_FindIndexByData(entry[1]);
+            break;
+
+        case 3:
+            data = entry[1];
+            index = -1;
+            if (data != 0) {
+                index = Inv_FindIndexByData(data);
+            }
+            D_800C0E22[0] = index;
+            break;
+
+        case 4:
+            *(s16 *)(entry[1] + 0xA) = entry[5];
+            *(s16 *)(entry[2] + 0xA) = entry[7];
+            *(s16 *)(entry[3] + 0xA) = entry[8];
+            break;
+        }
+    }
+}
 
 void BattleCmd_ResetTableCursor(void) {
     g_BattleCmdStackTop = g_BattleCmdStack;
--- src/main/psyq/misc34.c
+++ src/main/psyq/misc34.c
@@ -1,3 +1,5 @@
+/* MASPSX_FLAGS: --stack-return-delay */
+
 #include "include_asm.h"
 
 /* Function body emitted as asm() (PSYQ GCC 2.7.2 register
@@ -14,6 +16,48 @@ void __maspsx_include_asm_hack_DS_newmedia(void);
  * hints + `as -G8`. See tools/maspsx/PE1_IMPROVEMENTS.md. */
 INCLUDE_ASM("asm/USA/main/nonmatchings/psyq/misc34", DS_newmedia);
 
-INCLUDE_ASM("asm/USA/main/nonmatchings/psyq/misc34", DS_searchdir);
+int strcmp(char *a, char *b);
+
+extern int D_800A3CB4[];
+extern char D_800A3CBC[];
+
+int DS_searchdir(int dev, char *name) {
+    register int saved_dev asm("$19");
+    register char *saved_name asm("$20");
+    register int i asm("$16");
+    register int offset asm("$17");
+    register char *entry_name asm("$18");
+    register int value asm("$2");
+
+    saved_dev = dev;
+    saved_name = name;
+    i = 0;
+    entry_name = D_800A3CBC;
+    offset = 0;
+    do {
+        asm volatile(
+            "lui %0,%%hi(D_800A3CB4)\n\t"
+            "addu %0,%0,%1\n\t"
+            "lw %0,%%lo(D_800A3CB4)(%0)"
+            : "=r"(value)
+            : "r"(offset));
+
+        if (value == 0) {
+            return -1;
+        }
+        if (value == saved_dev) {
+            if (strcmp(saved_name, entry_name) == 0) {
+                return i + 1;
+            }
+            entry_name += 0x2C;
+        } else {
+            entry_name += 0x2C;
+        }
+        i++;
+        offset += 0x2C;
+    } while (i < 0x80);
+
+    return -1;
+}
 
 INCLUDE_ASM("asm/USA/main/nonmatchings/psyq/misc34", DS_cachefile);
