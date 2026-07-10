faf21496ee751f5d3e146fded292c97d7721bdb4
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-10 13:33:01 +0200

    Decompile read transfer setup

--- src/main/memcard/card_obj3.c
+++ src/main/memcard/card_obj3.c
@@ -1,3 +1,5 @@
+/* MASPSX_FLAGS: --stack-return-delay */
+
 #include "include_asm.h"
 
 #include "pe1/card_obj.h"
@@ -8,6 +10,9 @@ void CardObj_EmitCommand4C(CardObj *arg0, unsigned char arg1);
 void CardObj_EmitCommand46(CardObj *arg0, unsigned char arg1);
 void CardObj_EmitCommand47(CardObj *arg0, unsigned char arg1);
 void CardObj_EmitCommand4B(CardObj *arg0);
+extern int (*D_8009B740)(void);
+int CardObj_ProcessReadPayload(CardObj *arg0);
+void CardObj_EmitReadTransferCommand(CardObj *arg0);
 
 int CardObj_CalcReadPayloadSize(CardObj *arg0) {
     int first;
@@ -33,7 +38,44 @@ int CardObj_CalcReadPayloadSize(CardObj *arg0) {
     return first + base;
 }
 
-INCLUDE_ASM("asm/USA/main/nonmatchings/memcard/card_obj3", CardObj_BeginReadTransfer);
+int CardObj_BeginReadTransfer(CardObj *obj, int size) {
+    int total;
+    int bytes;
+    int extra;
+    int raw_e3;
+    int raw_e9;
+    register int ret asm("$2");
+    int four;
+
+    if (size == 0) {
+        return 0;
+    }
+    if (obj->field_04 != 0) {
+        return 0;
+    }
+    if (D_8009B740() == 0) {
+        ret = 1;
+        four = 4;
+        total = (size + 3) >> 2;
+        asm volatile("" : "=r"(four) : "0"(four));
+        obj->field_49 = four;
+        obj->field_46 = 1;
+        *(void **)((unsigned char *)obj + 0x14) = CardObj_EmitReadTransferCommand;
+        raw_e3 = obj->field_e3;
+        *(void **)((unsigned char *)obj + 0x18) = CardObj_ProcessReadPayload;
+        raw_e9 = obj->field_e9;
+        bytes = total << 2;
+        obj->field_00 = (unsigned char *)bytes;
+        obj->field_47 = 0;
+
+        bytes += ((raw_e3 + 1) >> 1) << 2;
+        extra = ((raw_e9 << 2) + raw_e9 + 3) & 0xFFC;
+        obj->field_04 = (unsigned char *)bytes;
+        obj->field_08 = (unsigned char *)(bytes + extra);
+        return ret;
+    }
+    return 0;
+}
 
 void CardObj_EmitReadTransferCommand(CardObj *arg0)
 {
