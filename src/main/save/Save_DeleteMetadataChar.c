1f79239b4240a980cb1c6d0437cc11a0215b88d2
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-10 10:23:34 +0200

    Decompile save metadata delete helper

---
+++ src/main/save/Save_DeleteMetadataChar.c
@@ -0,0 +1,28 @@
+/* CC1_FLAGS: -G8 */
+/* MASPSX_FLAGS: -G8 */
+
+extern int g_SaveMetadataWindowLength;
+extern unsigned char *g_SaveMetadataCursor;
+
+int Save_DeleteMetadataChar(void) {
+    register unsigned char *cursor asm("$4") = g_SaveMetadataCursor;
+    register int used asm("$3") = 0;
+    register int length asm("$5") = g_SaveMetadataWindowLength;
+
+    while (used < length && *cursor != 0xFF) {
+        used += *cursor < 0xFA;
+        cursor++;
+    }
+
+    used = 0 < used;
+    if (used != 0) {
+        cursor--;
+        cursor[0] = 0xFF;
+        cursor--;
+        if (cursor >= g_SaveMetadataCursor && *cursor >= 0xFA) {
+            cursor[0] = 0xFF;
+        }
+    }
+
+    return used;
+}
