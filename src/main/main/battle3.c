25b1b8ec6c686763d79d4d2e38f2dfc9d58c2ef0
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-10 20:03:01 +0200

    Decompile Battle_StartEncounter

--- src/main/main/battle3.c
+++ src/main/main/battle3.c
@@ -26,10 +26,31 @@ extern u16 D_8009D21C;
 extern struct { char _[16]; } D_800A7FF0_o __asm__("D_800A7FF0");
 extern u32 D_8009D28C;
 extern struct { char _[16]; } D_800B0CD8_o __asm__("D_800B0CD8");
+extern int D_8009D1E8;
+extern struct { char _[16]; } D_8009D290_o __asm__("D_8009D290");
+extern u8 D_8009CE7C;
+extern u8 D_8009CE78;
+extern u8 D_8009D288;
+extern struct { char _[16]; } D_8009D1AC_o __asm__("D_8009D1AC");
+extern struct { char _[16]; } D_8009D1A8_o __asm__("D_8009D1A8");
+extern struct { char _[16]; } D_8009D1CE_o __asm__("D_8009D1CE");
+extern struct { char _[16]; } D_8009D235_o __asm__("D_8009D235");
+extern struct { char _[16]; } D_800B8A90_o __asm__("D_800B8A90");
+extern struct { char _[16]; } D_8009D250_o __asm__("D_8009D250");
+extern struct { char _[16]; } D_8009D27C_o __asm__("D_8009D27C");
 #define D_8009D254 (*(void **)&D_8009D254_o)
 #define D_8009D1A0 (*(u32 *)&D_8009D1A0_o)
 #define D_800A7FF0_addr ((void *)&D_800A7FF0_o)
 #define D_800B0CD8 (*(u32 *)&D_800B0CD8_o)
+#define D_8009D290 (*(u32 *)&D_8009D290_o)
+#define D_8009D1AC (*(u32 *)&D_8009D1AC_o)
+#define D_8009D1A8 (*(u32 *)&D_8009D1A8_o)
+#define D_8009D1CE (*(u8 *)&D_8009D1CE_o)
+#define D_8009D235 (*(u8 *)&D_8009D235_o)
+#define D_800B8A90 ((u32 *)&D_800B8A90_o)
+#define D_8009D250 (*(u32 *)&D_8009D250_o)
+#define D_8009D27C (*(s16 *)&D_8009D27C_o)
+#define D_8009D1AC_BYTE (*(u8 *)&D_8009D1AC_o)
 
 void Battle_SaveAyaState(void);
 void Save_ResetGlobalFlags(void);
@@ -41,6 +62,11 @@ void Render_BeginSceneLoad(void);
 void Entity_SetActionMode(void *entity, int mode);
 int CD_StepReadState(int arg0);
 void Battle_SetupPlayerPalette(void);
+void srand(int seed);
+void Battle_SetupEnemyAnims(void);
+void Battle_CheckDropChance(void);
+void Window_SetBoundsByMode(int mode);
+int Entity_CheckActionIdMatch(void);
 
 /* Function body emitted as asm() (PSYQ GCC 2.7.2 register
  * allocation / scheduling diverges from the ROM pervasively).
@@ -144,7 +170,103 @@ void Battle_ResetEnemyStats(int mode) {
 
 INCLUDE_ASM("asm/USA/main/nonmatchings/main/battle3", Battle_SetupPlayerPalette);
 
-INCLUDE_ASM("asm/USA/main/nonmatchings/main/battle3", Battle_StartEncounter);
+void Battle_StartEncounter(int mode) {
+    register int i asm("$4");
+    register int neg_one asm("$5");
+    register void *entity asm("$2");
+    register void *actor asm("$3");
+    u32 value;
+    register int mode_reg asm("$16");
+
+    entity = D_8009D254;
+    D_8009D278 = *(Combatant **)entity;
+    D_8009D1E8 = 0;
+    D_8009D290 = 0;
+    D_8009D28C = 0;
+    D_8009CE7C = 0;
+    D_8009CE78 = 0;
+    D_8009D288 = 0;
+    D_8009CE74 = 0;
+    mode_reg = mode;
+    Save_ResetGlobalFlags();
+
+    i = 0;
+    neg_one = -1;
+    asm volatile(
+        ".set noat\n"
+        "lui $1, %%hi(D_8009D1AC)\n"
+        "sb $0, %%lo(D_8009D1AC)($1)\n"
+        "lui $3, %%hi(D_8009D1AC)\n"
+        "lw $3, %%lo(D_8009D1AC)($3)\n"
+        "addiu $2, $0, -0x301\n"
+        "lui $1, %%hi(D_8009D1A8)\n"
+        "sw $0, %%lo(D_8009D1A8)($1)\n"
+        "lui $1, %%hi(D_8009D1CE)\n"
+        "sb $0, %%lo(D_8009D1CE)($1)\n"
+        "lui $1, %%hi(D_8009D235)\n"
+        "sb $0, %%lo(D_8009D235)($1)\n"
+        "sw $0, %%gp_rel(D_8009D304)($gp)\n"
+        "sh $0, %%gp_rel(D_8009D21C)($gp)\n"
+        "and $3, $3, $2\n"
+        "lui $1, %%hi(D_8009D1AC)\n"
+        "sw $3, %%lo(D_8009D1AC)($1)\n"
+        ".set at"
+        :
+        : "r"(i), "r"(neg_one)
+        : "$1", "$2", "$3", "memory");
+
+    for (; (u8)i < 0xA; i++) {
+        *(u16 *)((u8 *)D_800A7FF0_addr + ((u8)i << 2)) = 0;
+        *(u16 *)((u8 *)D_800A7FF0_addr + ((u8)i << 2) + 2) = neg_one;
+    }
+
+    for (i = 0; (u8)i < 7; i++) {
+        D_800B8A90[(u8)i] = 0;
+    }
+
+    srand(D_8009D250);
+    Battle_ResetEnemyStats(0);
+    Battle_SetupEnemyAnims();
+
+    actor = D_8009D278;
+    value = *(u32 *)((u8 *)actor + 8);
+    if ((s32)value <= 0) {
+        *(u32 *)((u8 *)actor + 8) = 0x10000;
+    } else {
+        u32 max_value;
+
+        max_value = *(u32 *)((u8 *)actor + 0x28);
+        if ((s32)value >= (s32)max_value) {
+            *(u32 *)((u8 *)actor + 8) = max_value;
+            *(u32 *)((u8 *)actor + 0x34) = 0xF0;
+        }
+    }
+
+    Battle_CheckDropChance();
+    {
+        register void *post_entity asm("$3");
+        register void *post_actor asm("$4");
+        register u32 callback asm("$2");
+
+        post_entity = D_8009D254;
+        post_actor = *(void **)((u8 *)post_entity + 0x238);
+        callback = (u32)Entity_CheckActionIdMatch;
+        *(u32 *)((u8 *)post_entity + 0x194) = callback;
+        D_8009D27C = *(u32 *)((u8 *)post_actor + 0x18) - 0x64;
+    }
+    Window_SetBoundsByMode(mode_reg & 0xFF);
+    {
+        register void *final_actor asm("$2");
+        register void *final_entity asm("$4");
+
+        final_actor = D_8009D278;
+        asm volatile(
+            "lui %0, %%hi(D_8009D254)\n"
+            "lw %0, %%lo(D_8009D254)(%0)"
+            : "=r"(final_entity));
+        Entity_SetActionMode(final_entity, *(u8 *)((u8 *)final_actor + 0x12));
+    }
+}
 
 INCLUDE_ASM("asm/USA/main/nonmatchings/main/battle3", Battle_Update);
 
