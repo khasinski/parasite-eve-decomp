63e1f6c642002ce9d5902415bbf239d5df1bcb56
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-11 18:58:28 +0200

    Decompile GTE translation matrix helper

--- src/main/psyq/libgte/mtx_07.c
+++ src/main/psyq/libgte/mtx_07.c
@@ -1,9 +1,13 @@
-void *TransMatrix(void *m, void *v) {
-    asm("lw $8,0($5)");
-    asm("lw $9,4($5)");
-    asm("lw $10,8($5)");
-    asm("sw $8,20($4)");
-    asm("sw $9,24($4)");
-    asm("sw $10,28($4)");
-    asm("addu $2,$4,$0");
+void *TransMatrix(int *m, int *v) {
+    register int x asm("$8") = v[0];
+    register int y asm("$9") = v[1];
+    register int z asm("$10") = v[2];
+    register int *ret asm("$2");
+
+    m[5] = x;
+    m[6] = y;
+    m[7] = z;
+    asm volatile("" ::: "memory");
+    asm volatile("addu %0,%1,$0" : "=r"(ret) : "r"(m));
+    return ret;
 }
