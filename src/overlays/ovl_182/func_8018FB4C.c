58bc32e90c84dc281fad7e82bea66dff979e6cf9
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-11 22:21:17 +0200

    Decompile ovl_181 ovl_183 field updater

---
+++ src/overlays/ovl_182/func_8018FB4C.c
@@ -0,0 +1,19 @@
+typedef signed short s16;
+typedef unsigned short u16;
+
+void func_8018FB4C(void *arg0, char *arg1, char *arg2) {
+    u16 phase_y;
+
+    *(u16 *)(arg2 + 0x10) += 0xF;
+    if (*(s16 *)(arg1 + 0x2) < 0x10) {
+        phase_y = *(volatile u16 *)(arg2 + 0x12);
+        phase_y += 8;
+    } else {
+        phase_y = *(volatile u16 *)(arg2 + 0x12);
+    }
+    *(u16 *)(arg2 + 0x12) = phase_y - 4;
+    *(u16 *)(arg2 + 0xA) += 0x64;
+    if (*(s16 *)(arg2 + 0x12) <= 0) {
+        arg1[1] = 2;
+    }
+}
