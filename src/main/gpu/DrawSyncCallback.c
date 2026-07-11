a24d8870e96810f6a897f9c742295cbd29e5cf2d
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-11 19:12:36 +0200

    Decompile draw sync callback

--- src/main/gpu/DrawSyncCallback.c
+++ src/main/gpu/DrawSyncCallback.c
@@ -1,20 +1,22 @@
 /* MASPSX_FLAGS: --stack-return-delay */
 
-extern unsigned char g_GraphDebug[];
-extern void (*g_GpuDebugPrintf[])(char *msg, int arg0);
+extern unsigned char D_8009574E[];
+extern void (*D_80095748[])(char *msg, int arg0);
 extern char D_80011854[];
 
 int DrawSyncCallback(int arg0) {
-    unsigned char *base;
+    register unsigned char *base asm("$16");
     register int saved asm("$17");
     int old;
 
-    base = g_GraphDebug;
+    base = D_8009574E;
+    asm volatile("" : "=r"(base) : "0"(base));
     saved = arg0;
     if (base[0] >= 2) {
-        g_GpuDebugPrintf[0](D_80011854, saved);
+        D_80095748[0](D_80011854, saved);
     }
 
+    asm volatile("" : "=r"(base) : "0"(base));
     old = *(int *)(base + 0xA);
     *(int *)(base + 0xA) = saved;
     return old;
