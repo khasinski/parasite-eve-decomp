1c81e966a95fa6151dd9a89013952e8b14558d05
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-11 12:55:45 +0200

    Decompile room setup callbacks

---
+++ src/overlays/room_m344/func_8018F094.c
@@ -0,0 +1,10 @@
+typedef int s32;
+
+extern char D_80192B7C;
+
+s32 func_800C2414(void *arg0, void *arg1);
+
+s32 func_8018F094(void *arg0) {
+    func_800C2414(arg0, &D_80192B7C);
+    return 0;
+}
