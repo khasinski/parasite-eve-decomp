abf47e85c139c2682d0ade00a2fdaf541b37d7f7
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-11 22:07:08 +0200

    Decompile overlay arg setters

---
+++ src/overlays/ovl_182/func_801919EC.c
@@ -0,0 +1,6 @@
+#include "../room_lib/room_lib.h"
+
+extern int D_80191BD0;
+extern int D_80191BD4;
+
+ROOMLIB_SET_ARGS2(func_801919EC, D_80191BD0, D_80191BD4)
---
+++ src/overlays/ovl_190/func_80193120.c
@@ -0,0 +1,7 @@
+#include "../room_lib/room_lib.h"
+
+extern int D_8019328C;
+extern int D_80193290;
+extern int D_80193294;
+
+ROOMLIB_SET_ARGS3(func_80193120, D_8019328C, D_80193290, D_80193294)
