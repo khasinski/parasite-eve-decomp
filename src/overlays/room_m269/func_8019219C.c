c910b8177a95238ca6349327b7bffd86300e0e62
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-11 12:51:34 +0200

    Decompile room slot getters

---
+++ src/overlays/room_m269/func_8019219C.c
@@ -0,0 +1,5 @@
+extern int RoomSlotA_8019218C;
+
+int *func_8019219C(void) {
+    return &RoomSlotA_8019218C;
+}
---
+++ src/overlays/room_m318/func_8019938C.c
@@ -0,0 +1,5 @@
+extern int D_801998F0;
+
+int *func_8019938C(void) {
+    return &D_801998F0;
+}
