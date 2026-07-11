4b18d457559f47f5faec236b067a3b3fb048d002
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-11 03:43:21 +0200

    Decompile additional render and audio helpers

--- src/main/gte/Gte_NormalizeVecS32toS16.c
+++ src/main/gte/Gte_NormalizeVecS32toS16.c
@@ -1,40 +1,92 @@
-/* Hand-written GTE vector helper island.
- * These labels share a tail and temporarily park $ra in $a3, which is not
- * normal compiler C output.
- */
-asm(".text");
-asm(".set noreorder");
-asm(".set noat");
-asm(".globl Gte_NormalizeVecS32toS16");
-asm("Gte_NormalizeVecS32toS16:");
-asm(".byte 0x00, 0x00, 0x88, 0x8C");
-asm(".byte 0x04, 0x00, 0x89, 0x8C");
-asm(".byte 0x08, 0x00, 0x8A, 0x8C");
-asm(".byte 0x10, 0x00, 0x00, 0x10");
-asm(".byte 0x00, 0x00, 0x00, 0x00");
-asm(".globl Gte_NormalizeVec");
-asm("Gte_NormalizeVec:");
-asm(".byte 0x00, 0x00, 0x88, 0x8C");
-asm(".byte 0x04, 0x00, 0x89, 0x8C");
-asm(".byte 0x08, 0x00, 0x8A, 0x8C");
-asm(".byte 0x21, 0x38, 0xE0, 0x03");
-asm(".byte 0x65, 0xE0, 0x01, 0x0C");
-asm(".byte 0x00, 0x00, 0x00, 0x00");
-asm(".byte 0x00, 0x00, 0xA8, 0xAC");
-asm(".byte 0x04, 0x00, 0xA9, 0xAC");
-asm(".byte 0x08, 0x00, 0xAA, 0xAC");
-asm(".byte 0x21, 0xF8, 0xE0, 0x00");
-asm(".byte 0x08, 0x00, 0xE0, 0x03");
-asm(".byte 0x00, 0x00, 0x00, 0x00");
-asm(".byte 0x00, 0x00, 0x88, 0x84");
-asm(".byte 0x02, 0x00, 0x89, 0x84");
-asm(".byte 0x04, 0x00, 0x8A, 0x84");
-asm(".byte 0x21, 0x38, 0xE0, 0x03");
-asm(".byte 0x65, 0xE0, 0x01, 0x0C");
-asm(".byte 0x00, 0x00, 0x00, 0x00");
-asm(".byte 0x00, 0x00, 0xA8, 0xA4");
-asm(".byte 0x02, 0x00, 0xA9, 0xA4");
-asm(".byte 0x04, 0x00, 0xAA, 0xA4");
-asm(".byte 0x21, 0xF8, 0xE0, 0x00");
-asm(".byte 0x08, 0x00, 0xE0, 0x03");
-asm(".byte 0x00, 0x00, 0x00, 0x00");
+typedef signed short s16;
+typedef int s32;
+typedef unsigned int u32;
+typedef long long s64;
+typedef unsigned long long u64;
+
+typedef struct Vec3i {
+    s32 x;
+    s32 y;
+    s32 z;
+} Vec3i;
+
+typedef struct Vec3s {
+    s16 x;
+    s16 y;
+    s16 z;
+} Vec3s;
+
+int Gte_ISqrt(int value);
+
+static u64 Gte_DivU64ByU32(u64 numerator, u32 denominator) {
+    u64 quotient;
+    u32 remainder;
+    s32 i;
+
+    quotient = 0;
+    remainder = 0;
+    for (i = 0; i < 64; i++) {
+        remainder <<= 1;
+        remainder |= numerator >> 63;
+        numerator <<= 1;
+        quotient <<= 1;
+
+        if (remainder >= denominator) {
+            remainder -= denominator;
+            quotient |= 1;
+        }
+    }
+
+    return quotient;
+}
+
+static s32 Gte_NormalizeComponent(s32 value, s32 length) {
+    u64 magnitude;
+    u64 quotient;
+
+    if (length == 0) {
+        return 0;
+    }
+
+    if (value < 0) {
+        magnitude = (u64)-((s64)value);
+    } else {
+        magnitude = value;
+    }
+
+    quotient = Gte_DivU64ByU32(magnitude << 12, length);
+    if (quotient > 0x7FFFFFFF) {
+        quotient = 0x7FFFFFFF;
+    }
+
+    if (value < 0) {
+        return -(s32)quotient;
+    }
+    return (s32)quotient;
+}
+
+void Gte_NormalizeVec(Vec3i *src, Vec3i *dst) {
+    s32 x;
+    s32 y;
+    s32 z;
+    s32 length;
+
+    x = src->x;
+    y = src->y;
+    z = src->z;
+    length = Gte_ISqrt((x * x) + (y * y) + (z * z));
+
+    dst->x = Gte_NormalizeComponent(x, length);
+    dst->y = Gte_NormalizeComponent(y, length);
+    dst->z = Gte_NormalizeComponent(z, length);
+}
+
+void Gte_NormalizeVecS32toS16(Vec3i *src, Vec3s *dst) {
+    Vec3i normalized;
+
+    Gte_NormalizeVec(src, &normalized);
+
+    dst->x = normalized.x;
+    dst->y = normalized.y;
+    dst->z = normalized.z;
+}
