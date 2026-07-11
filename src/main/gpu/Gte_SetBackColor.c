17477d1e0e2cf115fa3d78d296eaa41d4fa9061e
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-11 16:41:19 +0200

    Decompile GTE back color helper

--- src/main/gpu/Gte_SetBackColor.c
+++ src/main/gpu/Gte_SetBackColor.c
@@ -1,16 +1,24 @@
 typedef unsigned short u16;
 
 void Gte_SetBackColor(u16 *dst, int r, int g, int b) {
-    int i;
+    register int sr asm("t4");
+    register int sg asm("t5");
+    register int sb asm("t6");
 
-    r <<= 4;
-    g <<= 4;
-    b <<= 4;
-    asm volatile("ctc2 %0,$13" : : "r"(r));
-    asm volatile("ctc2 %0,$14" : : "r"(g));
-    asm volatile("ctc2 %0,$15" : : "r"(b));
+    sr = r << 4;
+    sg = g << 4;
+    sb = b << 4;
+    asm volatile("ctc2 %0,$13" : : "r"(sr));
+    asm volatile("ctc2 %0,$14" : : "r"(sg));
+    asm volatile("ctc2 %0,$15" : : "r"(sb));
 
-    for (i = 0; i < 9; i++) {
-        dst[i] = 0;
-    }
+    dst[0] = 0;
+    dst[1] = 0;
+    dst[2] = 0;
+    dst[3] = 0;
+    dst[4] = 0;
+    dst[5] = 0;
+    dst[6] = 0;
+    dst[7] = 0;
+    dst[8] = 0;
 }
