e6fdac123bf4e37e940fc4cf4430ac31a0ca035d
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-11 04:44:59 +0200

    Decompile Math_Shift64

---
+++ src/main/main/Math_Shift64.c
@@ -0,0 +1,32 @@
+typedef unsigned int u32;
+
+typedef struct MathU64 {
+    u32 lo;
+    u32 hi;
+} MathU64;
+
+MathU64 *Math_Shift64(MathU64 *out, int direction, u32 lo, u32 hi, int amount) {
+    int i;
+
+    if (direction == 0) {
+        for (i = 0; i < amount; i++) {
+            hi <<= 1;
+            if (lo & 0x80000000) {
+                hi |= 1;
+            }
+            lo <<= 1;
+        }
+    } else {
+        for (i = 0; i < amount; i++) {
+            lo >>= 1;
+            if (hi & 1) {
+                lo |= 0x80000000;
+            }
+            hi >>= 1;
+        }
+    }
+
+    out->lo = lo;
+    out->hi = hi;
+    return out;
+}
