c2e3e7aa76ba5c27cf040fe402260691edea285b
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-11 22:36:21 +0200

    Decompile ovl_178 vector initializer

---
+++ src/overlays/ovl_178/func_80191838.c
@@ -0,0 +1,10 @@
+typedef signed short s16;
+
+void func_80191838(void *arg0, void *arg1, char *arg2) {
+    *(s16 *)(arg2 + 0x0) = 0;
+    *(s16 *)(arg2 + 0x2) = 0;
+    *(s16 *)(arg2 + 0x4) = 0;
+    *(s16 *)(arg2 + 0x8) = 0x400;
+    *(s16 *)(arg2 + 0xA) = 0;
+    *(s16 *)(arg2 + 0xC) = 0;
+}
