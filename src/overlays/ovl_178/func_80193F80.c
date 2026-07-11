ea98e688018566040ebe00751f6e9b94025793f7
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-11 23:33:56 +0200

    Decompile ovl_178 timer tick helper

---
+++ src/overlays/ovl_178/func_80193F80.c
@@ -0,0 +1,23 @@
+int func_800C6C18(void);
+int func_800C2B68(void);
+
+void func_80193F80(void *arg0, unsigned char *arg1, char *arg2) {
+    register char *timer __asm__("a1") = arg2;
+    short count = *(short *)(arg2 + 0x2A);
+
+    if (count != 0) {
+        *(short *)(arg2 + 0x2A) = count - 1;
+    }
+
+    if (*(short *)(timer + 0x2C) == 1) {
+        *(short *)(timer + 0x2C) = 0;
+        if (*(short *)(timer + 0x2A) == 0) {
+            *(short *)(timer + 0x2A) = *(unsigned short *)(timer + 0x28);
+            func_800C6C18();
+        }
+    }
+
+    if (func_800C2B68() == 1) {
+        arg1[1] = 2;
+    }
+}
