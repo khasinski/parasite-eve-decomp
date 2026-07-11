f0b00385d63a070f85a9a848f4ea7a14f2d5e998
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-11 22:17:37 +0200

    Decompile ovl_174 ovl_175 event handlers

---
+++ src/overlays/ovl_175/func_8018F0BC.c
@@ -0,0 +1,11 @@
+int func_800C6CE0(void);
+int func_800C2414(void *arg0, void *arg1);
+
+extern char D_80191F94[];
+
+int func_8018F0BC(void *arg0) {
+    if (func_800C6CE0() == 3) {
+        func_800C2414(arg0, D_80191F94);
+    }
+    return 0;
+}
