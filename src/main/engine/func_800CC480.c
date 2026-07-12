2899cf4d3a4d9f16fd7d589e8c35717222d66e55
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 11:17:20 +0200

    Decompile func_800CC480

--- src/main/engine/func_800CC480.c
+++ src/main/engine/func_800CC480.c
@@ -17,26 +17,34 @@ extern short D_800F3458;
 
 int func_800CC480(void *arg0, u8 *params, u8 *anim) {
     register short *base_a0 asm("$4");
-    int value;
+    register int init_a0 asm("$4") = 3;
+    register u8 *anim_s1 asm("$17") = anim;
+    register int value_v0 asm("$2");
+    register int value_v1 asm("$3");
 
-    func_800C2EAC(3);
+    func_800C2EAC(init_a0);
     func_800C3098(0x10);
     func_800C2FF0(0x20, 0x20);
     func_800C3238(2);
 
-    value = *(u16 *)(params + 0x2);
-    value = 0x80 - (value * 2);
-    if ((value << 16) < 0) {
-        value = 0;
+    value_v1 = *(u16 *)(params + 0x2);
+    value_v0 = 0x80;
+    value_v1 <<= 1;
+    value_v0 -= value_v1;
+    asm volatile("addu %0, %1, $zero" : "=r"(value_v1) : "r"(value_v0));
+    value_v0 <<= 16;
+    if (value_v0 < 0) {
+        value_v1 = 0;
     }
 
     base_a0 = &D_800F3458;
-    base_a0[0] = value;
-    base_a0[-20] = *(u16 *)(anim + 0x6);
-    D_800F3432 = *(u16 *)(anim + 0x8);
-    D_800F3434 = *(u16 *)(anim + 0xA);
-    D_800F3440 = *(short *)(anim + 0x4);
-    D_800F3444 = *(short *)(anim + 0x4) * 2;
-    D_800F3448 = *(short *)(anim + 0x4);
+    base_a0[0] = value_v1;
+    asm volatile("" : "=r"(base_a0) : "0"(base_a0));
+    base_a0[-20] = *(u16 *)(anim_s1 + 0x6);
+    D_800F3432 = *(u16 *)(anim_s1 + 0x8);
+    D_800F3434 = *(u16 *)(anim_s1 + 0xA);
+    D_800F3440 = *(short *)(anim_s1 + 0x4);
+    D_800F3444 = *(short *)(anim_s1 + 0x4) * 2;
+    D_800F3448 = *(short *)(anim_s1 + 0x4);
     func_800C3B04(base_a0 - 20);
 }
