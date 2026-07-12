04cf2e386a8b8c7688d29e112b97f188f9317e01
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 21:22:21 +0200

    Decompile more room state dispatchers

---
+++ src/overlays/room_m318/func_801902B8.c
@@ -0,0 +1,5 @@
+#include "../room_lib/room_lib.h"
+
+extern void RoomLib_ResetAndSignalB_801909F8(RoomEnt *o);
+
+ROOMLIB_STATE_DISPATCH_VARIANT2(func_801902B8, RoomLib_ResetAndSignalB_801909F8)
---
+++ src/overlays/room_m383/func_801949D4.c
@@ -0,0 +1,5 @@
+#include "../room_lib/room_lib.h"
+
+extern void RoomLib_ResetAndSignalB_80195114(RoomEnt *o);
+
+ROOMLIB_STATE_DISPATCH_VARIANT2(func_801949D4, RoomLib_ResetAndSignalB_80195114)
