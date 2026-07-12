25ae200343c42244b0c25629e7fafcbeda3af6bd
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 17:37:56 +0200

    Decompile museum timer args group

---
+++ src/overlays/room_m309/func_80192DD4.c
@@ -0,0 +1,9 @@
+#include "../room_lib/room_lib.h"
+
+extern void RoomLib_RearmOnMatch_80192E9C(void);
+
+ROOMLIB_INIT_TIMER_ARGS(func_80192DD4, RoomLib_RearmOnMatch_80192E9C, 0x08064B99)
+
+int func_80192E6C(void) {
+    return 0;
+}
