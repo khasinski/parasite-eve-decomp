9bfda7ac09ccbe7c05c6d3c45514b54d5236dee7
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 21:27:23 +0200

    Decompile room dispatchers in m075 m080 m082

---
+++ src/overlays/room_m080/func_80190F38.c
@@ -0,0 +1,5 @@
+#include "../room_lib/room_lib.h"
+
+extern void func_801918D0(RoomEnt *o);
+
+ROOMLIB_STATE_DISPATCH_VARIANT2(func_80190F38, func_801918D0)
---
+++ src/overlays/room_m082/func_80191C78.c
@@ -0,0 +1,5 @@
+#include "../room_lib/room_lib.h"
+
+extern void RoomLib_ResetAndSignalB_801923B8(RoomEnt *o);
+
+ROOMLIB_STATE_DISPATCH_VARIANT2(func_80191C78, RoomLib_ResetAndSignalB_801923B8)
