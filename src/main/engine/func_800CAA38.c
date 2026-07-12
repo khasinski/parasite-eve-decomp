eaa0b84b2251d05f0a6214591503f854f9704077
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 12:00:41 +0200

    Decompile func_800CAA38

--- src/main/engine/func_800CAA38.c
+++ src/main/engine/func_800CAA38.c
@@ -18,19 +18,27 @@ extern u16 D_800E2362;
 extern u16 D_800E2364;
 
 int func_800CAA38(void *arg0, void *arg1, u8 *anim) {
-    register u8 *anim_s0 asm("$16") = anim;
-    register u16 *field_s1 asm("$17") = &D_800E2360;
+    register u8 *anim_s0 asm("$16");
+    register u16 *field_s1 asm("$17");
     register SVECTOR *out_s2 asm("$18");
+    register int y_base_v0 asm("$2");
+    register u16 y_out_v1 asm("$3");
+    register int z_base_v1 asm("$3");
+    register u16 z_out_a0 asm("$4");
     SVECTOR in0;
     SVECTOR in1;
     SVECTOR out;
     char *data;
+
+    anim_s0 = anim;
+    asm volatile("" : "=r"(anim_s0) : "0"(anim_s0));
 
     in0 = D_800C21C4;
     in1 = D_800C21CC;
     out_s2 = &out;
 
     ApplyMatrixSV(*(char **)(D_800E27A8 + 0x238) + 0x260, &in0, out_s2);
+    field_s1 = &D_800E2360;
     *(u16 *)(anim_s0 + 0x8) = field_s1[0] + out.vx;
     *(u16 *)(anim_s0 + 0xA) = D_800E2362 + out.vy;
     data = D_800E27A8;
@@ -38,8 +46,13 @@ int func_800CAA38(void *arg0, void *arg1, u8 *anim) {
 
     ApplyMatrixSV(*(char **)(data + 0x238) + 0x260, &in1, out_s2);
     *(u16 *)(anim_s0 + 0x10) = field_s1[0] + out.vx;
-    *(u16 *)(anim_s0 + 0x12) = D_800E2362 + out.vy;
+    y_base_v0 = D_800E2362;
+    asm volatile("" : "=r"(y_base_v0) : "0"(y_base_v0));
+    y_out_v1 = out.vy;
+    *(u16 *)(anim_s0 + 0x12) = y_base_v0 + y_out_v1;
+    z_base_v1 = D_800E2364;
+    z_out_a0 = out.vz;
     *(u16 *)(anim_s0 + 0x4) = 0x7F;
     *(u16 *)(anim_s0 + 0x6) = 0;
-    *(u16 *)(anim_s0 + 0x14) = D_800E2364 + out.vz;
+    *(u16 *)(anim_s0 + 0x14) = z_base_v1 + z_out_a0;
 }
