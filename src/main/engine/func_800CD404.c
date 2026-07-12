eedd523899c28ad18fea885520a6893be0d5779e
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 13:53:04 +0200

    Decompile func_800CD404

--- src/main/engine/func_800CD404.c
+++ src/main/engine/func_800CD404.c
@@ -1,3 +1,5 @@
+/* CC1_FLAGS: -fno-schedule-insns */
+
 typedef unsigned char u8;
 typedef signed short s16;
 
@@ -23,10 +25,15 @@ int func_800CD404(void *arg0, void *arg1, u8 *anim) {
     register u8 *anim_s2 asm("$18") = anim;
     register int *field_s1 asm("$17");
     register u8 *color_a3 asm("$7");
+    register int *position_v0 asm("$2");
+    register u8 *call_a0 asm("$4");
+    register int *call_a1 asm("$5");
+    register int call_a2 asm("$6");
+    register int value_v0 asm("$2");
+    register int value_v1 asm("$3");
+    register int entry_v0 asm("$2");
     int i;
     int shade;
-    u8 *entry;
-    int *position;
 
     func_800C2EAC(3);
     func_800C3098(0x10);
@@ -34,24 +41,41 @@ int func_800CD404(void *arg0, void *arg1, u8 *anim) {
     func_800C3238(2);
 
     color_a3 = &D_800E2254;
-    position = &D_800F33D4;
-    position[0] = *(s16 *)(anim_s2 + 0x8);
-    position[1] = *(s16 *)(anim_s2 + 0xA);
-    position[2] = *(s16 *)(anim_s2 + 0xC);
+    asm volatile("" : "=r"(color_a3) : "0"(color_a3));
+    call_a0 = color_a3 - 4;
+    asm volatile("" : "=r"(call_a0) : "0"(call_a0));
+    position_v0 = &D_800F33D4;
+    value_v1 = *(s16 *)(anim_s2 + 0x8);
+    call_a1 = position_v0 - 5;
+    position_v0[0] = value_v1;
+    value_v0 = *(s16 *)(anim_s2 + 0xA);
+    call_a2 = 1;
+    D_800F33D8 = value_v0;
+    value_v0 = *(s16 *)(anim_s2 + 0xC);
+    i = 0;
+    D_800F33DC = value_v0;
     shade = anim_s2[3];
+    asm volatile("" ::: "$17");
     field_s1 = &D_800F32C4;
     *color_a3 = (shade * 2) - 0x80;
-    func_800C42A4(color_a3 - 4, position - 5, 1);
+    func_800C42A4(call_a0, call_a1, call_a2);
 
     func_800C3238(3);
 
-    i = 0;
     do {
-        entry = anim_s2 + ((i & 0xFFFF) * 8);
-        field_s1[0] = *(s16 *)(entry + 0x10);
-        field_s1[1] = *(s16 *)(entry + 0x12);
-        field_s1[2] = *(s16 *)(entry + 0x14);
+        call_a0 = &D_800F3460;
+        asm volatile("" : "=r"(call_a0) : "0"(call_a0));
+        entry_v0 = (int)(anim_s2 + ((i & 0xFFFF) * 8));
+        value_v1 = *(s16 *)(entry_v0 + 0x10);
+        call_a1 = field_s1 - 5;
+        field_s1[0] = value_v1;
+        value_v1 = *(s16 *)(entry_v0 + 0x12);
+        call_a2 = 1;
+        field_s1[1] = value_v1;
+        value_v0 = *(s16 *)(entry_v0 + 0x14);
+        asm volatile("" : "=r"(value_v0) : "0"(value_v0));
+        field_s1[2] = value_v0;
         i++;
-        func_800C42A4(&D_800F3460, field_s1 - 5, 1);
-    } while ((i & 0xFFFF) < 2);
+        func_800C42A4(call_a0, call_a1, call_a2);
+    } while ((i & 0xFFFFU) < 2);
 }
