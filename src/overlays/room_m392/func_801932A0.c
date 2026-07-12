89d2f17b177287e2251d27ad95327a93a7e9613f
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 18:03:37 +0200

    Decompile additional room timer parsers

---
+++ src/overlays/room_m392/func_801932A0.c
@@ -0,0 +1,9 @@
+#include "../room_lib/room_lib.h"
+
+extern void RoomLib_RearmOnMatch_80193368(void);
+
+ROOMLIB_INIT_TIMER_ARGS(func_801932A0, RoomLib_RearmOnMatch_80193368, 0x08064CCC)
+
+int func_80193338(void) {
+    return 0;
+}
