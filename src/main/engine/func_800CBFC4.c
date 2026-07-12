326b1ad5151924a8163811573426c19192b7dd7d
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 12:03:08 +0200

    Decompile func_800CBFC4

--- src/main/engine/func_800CBFC4.c
+++ src/main/engine/func_800CBFC4.c
@@ -12,14 +12,23 @@ extern u16 D_800E2292;
 extern u16 D_800E2294;
 
 int func_800CBFC4(void *arg0, void *arg1, u8 *anim) {
+    register int x_base_a1 asm("$5");
+    register int z_base_a1 asm("$5");
+    int x_rand;
+    int z_rand;
+
     D_800F3450 = *func_800C2B10(0);
     D_800F3451 = *func_800C2B10(1);
     D_800F3452 = *func_800C2B10(2);
 
-    *(u16 *)(anim + 0x6) = D_800E2290 + (rand() % 201) - 0x64;
+    x_rand = rand();
+    x_base_a1 = D_800E2290 - 0x64;
+    *(u16 *)(anim + 0x6) = x_base_a1 + (x_rand % 201);
     *(u16 *)(anim + 0x8) = D_800E2292;
 
+    z_rand = rand();
+    z_base_a1 = D_800E2294 - 0x64;
     *(u16 *)(anim + 0x4) = 0x20C;
     anim[3] = 0x7F;
-    *(u16 *)(anim + 0xA) = D_800E2294 + (rand() % 201) - 0x64;
+    *(u16 *)(anim + 0xA) = z_base_a1 + (z_rand % 201);
 }
