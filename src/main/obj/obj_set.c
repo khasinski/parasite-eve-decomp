d7b69ca78a9216587927c9553e8ddb83fa1cbaf7
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 12:32:00 +0200

    Decompile obj_set

--- src/main/obj/obj_set.c
+++ src/main/obj/obj_set.c
@@ -1,17 +1,22 @@
 #include "pe1/geom_state.h"
 
+extern GeomState * volatile D_800B1624 __asm__("D_800B1624");
+
 int Obj_SetEntrySlotValue(int index, int slot, u8 value) {
-    GeomCtrlEntry *entry = (GeomCtrlEntry *)((u8 *)g_GeomState + g_GeomState->ctrl_offset + (index << 4));
-    u8 *ptr;
+    GeomCtrlEntry *entry = (GeomCtrlEntry *)((u8 *)D_800B1624 + D_800B1624->ctrl_offset + (index << 4));
+    register u8 *ptr __asm__("$5");
     u8 *base;
+    int ret;
+
     base = (u8 *)entry + entry->slot_offset;
-    ptr = base + (slot << 1);
+    ptr = (u8 *)((slot << 1) + (int)base);
+    ret = 0;
     ptr[1] = value;
-    return 0;
+    return ret;
 }
 
 int Obj_SetEntryFlags(int index, int bits) {
-    GeomCtrlEntry *entry = (GeomCtrlEntry *)((u8 *)g_GeomState + g_GeomState->ctrl_offset + (index << 4));
+    GeomCtrlEntry *entry = (GeomCtrlEntry *)((u8 *)D_800B1624 + D_800B1624->ctrl_offset + (index << 4));
     entry->head.b.flags |= bits & 0x30;
     return 0;
 }
