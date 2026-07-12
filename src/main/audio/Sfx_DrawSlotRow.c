f80d409fac327418b02638785d8455cc167728c8
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 12:26:31 +0200

    Decompile Sfx_DrawSlotRow

--- src/main/audio/Sfx_DrawSlotRow.c
+++ src/main/audio/Sfx_DrawSlotRow.c
@@ -18,16 +18,18 @@ void Draw_PrintNumberWidth3Unk(int value);
 void Sfx_DrawSlotRow(u8 *entry, u8 *text) {
     int type;
     int sprite;
+    int ammo;
 
     Draw_StatePush();
 
     if (entry[6] < 8) {
         sprite = entry[9] + S16(entry, 0x12);
+        ammo = U16(entry, 0xA);
         if (sprite < 1000) {
-            if (U16(entry, 0xA) == sprite) {
+            if (ammo == sprite) {
                 Draw_AllocSprite(0x69);
             }
-        } else if (U16(entry, 0xA) == 999) {
+        } else if (ammo == 999) {
             Draw_AllocSprite(0x69);
         }
     }
@@ -40,28 +42,32 @@ void Sfx_DrawSlotRow(u8 *entry, u8 *text) {
     }
 
     type = entry[6];
-    if (type != 0 && type < 8) {
+    if (type != 0 && (unsigned int)type < 8) {
         if (type - 4 <= 0) {
             goto draw_ammo;
         }
-        if (type == 4) {
+        if (type != 4) {
+            goto draw_ammo;
+        }
+        goto done;
+    } else {
+        register int cmp __asm__("$2");
+        if ((unsigned int)type < 0x13 || (cmp = type - 0x12) == 0) {
             goto done;
         }
-    } else if (type < 0x13 || type == 0x12) {
-        goto done;
     }
 
 draw_ammo:
     Draw_OffsetCursor(0x56, 0);
 
     type = entry[6];
-    if (type != 0 && type < 8) {
+    if (type != 0 && (unsigned int)type < 8) {
         if (type - 4 > 0) {
             sprite = type + 0x1A;
         } else {
             sprite = 0x1F;
         }
-    } else if (type < 0x13) {
+    } else if ((unsigned int)type < 0x13) {
         sprite = 0x1E;
     } else {
         sprite = type + 0xC;
