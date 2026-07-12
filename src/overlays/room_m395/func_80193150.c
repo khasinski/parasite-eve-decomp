0d54f1778b42b518bdf599641e9a25b484e687a9
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 17:55:47 +0200

    Decompile Chrysler room timer handlers

---
+++ src/overlays/room_m395/func_80193150.c
@@ -0,0 +1,9 @@
+#include "../room_lib/room_lib.h"
+
+extern void RoomLib_RearmOnMatch_80193218(void);
+
+ROOMLIB_INIT_TIMER_ARGS(func_80193150, RoomLib_RearmOnMatch_80193218, 0x08064C78)
+
+int func_801931E8(void) {
+    return 0;
+}
