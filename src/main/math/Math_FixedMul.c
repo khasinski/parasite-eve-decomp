2f583f3469e2dafb1d10c2a0b456026f6a5ce68b
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 11:45:40 +0200

    Decompile Math_FixedMul

--- src/main/math/Math_FixedMul.c
+++ src/main/math/Math_FixedMul.c
@@ -1,5 +1,15 @@
-typedef long long s64;
-
 int Math_FixedMul(int a, int b) {
-    return (int)(((s64)a * b) >> 16);
+    register int lo asm("$2");
+    register int hi asm("$3");
+
+    asm volatile(
+        "mult %2,%3\n"
+        "mflo %0\n"
+        "mfhi %1"
+        : "=r"(lo), "=r"(hi)
+        : "r"(a), "r"(b));
+
+    lo = (unsigned int)lo >> 16;
+    hi <<= 16;
+    return hi | lo;
 }
