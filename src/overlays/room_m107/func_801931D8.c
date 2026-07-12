9cc4d836935e6e807785b06aa50930897f0a2733
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 17:42:28 +0200

    Decompile Eve concert timer args group

---
+++ src/overlays/room_m107/func_801931D8.c
@@ -0,0 +1,9 @@
+#include "../room_lib/room_lib.h"
+
+extern void RoomLib_RearmOnMatch_801932A0(void);
+
+ROOMLIB_INIT_TIMER_ARGS(func_801931D8, RoomLib_RearmOnMatch_801932A0, 0x08064C9A)
+
+int func_80193270(void) {
+    return 0;
+}
