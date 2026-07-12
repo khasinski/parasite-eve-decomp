a29c20d58cbb2da953b6ea06fa4218b3fa93789e
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 13:57:27 +0200

    Decompile func_800C6D5C

--- src/main/engine/func_800C6D5C.c
+++ src/main/engine/func_800C6D5C.c
@@ -7,23 +7,28 @@ extern s16 D_800F3420;
 void func_800C6D5C(u8 *data, u8 xOffset, u8 yOffset) {
     register u8 *data_t0 asm("$8");
     register u8 *ptr_t2 asm("$10");
+    register int value_v0 asm("$2");
+    register int value_v1 asm("$3");
     register int x_a0 asm("$4");
     register int i_t1 asm("$9");
     register int count_a0 asm("$4");
     int total;
 
-    asm volatile("move %0,$4" : "=r"(data_t0));
+    asm volatile("" : "=r"(data_t0) : "0"(data));
+    value_v0 = *(u16 *)(data_t0 + 0xC);
+    asm volatile("" : "=r"(value_v0) : "0"(value_v0));
     x_a0 = xOffset & 0xFF;
     ptr_t2 = data_t0 + 0x10;
 
-    if (*(u16 *)(data_t0 + 0xC) != x_a0 || *(u16 *)(data_t0 + 0xE) != (yOffset & 0xFF)) {
+    if (value_v0 != x_a0 || *(u16 *)(data_t0 + 0xE) != (yOffset & 0xFF)) {
         i_t1 = 0;
         *(u16 *)(data_t0 + 0xC) = x_a0;
-        total = *(u16 *)(data_t0 + 0);
+        value_v0 = *(u16 *)(data_t0 + 0);
         count_a0 = *(u16 *)(data_t0 + 2);
-        total += count_a0;
-        *(u16 *)(data_t0 + 0xE) = yOffset & 0xFF;
-        if (total != 0) {
+        value_v1 = yOffset & 0xFF;
+        value_v0 += count_a0;
+        *(u16 *)(data_t0 + 0xE) = value_v1;
+        if (value_v0 != 0) {
             register u8 *ptr_a3 asm("$7");
 
             ptr_a3 = data_t0 + 0x1B;
@@ -43,14 +48,16 @@ void func_800C6D5C(u8 *data, u8 xOffset, u8 yOffset) {
             } while ((u16)i_t1 < total);
         }
 
-        total = *(u16 *)(data_t0 + 4);
-        count_a0 = *(u16 *)(data_t0 + 6);
-        total += count_a0;
         i_t1 = 0;
-        if (total != 0) {
+        asm volatile("" : "=r"(i_t1) : "0"(i_t1));
+        value_v0 = *(u16 *)(data_t0 + 4);
+        value_v1 = *(u16 *)(data_t0 + 6);
+        value_v0 += value_v1;
+        count_a0 = (int)ptr_t2;
+        if (value_v0 != 0) {
             register u8 *ptr_a3 asm("$7");
 
-            ptr_a3 = ptr_t2 + 0xF;
+            ptr_a3 = (u8 *)count_a0 + 0xF;
             do {
                 i_t1++;
                 ptr_a3[-7] += xOffset;
