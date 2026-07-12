453b84aa10c0b673e05ba554a9bba023ed3f0788
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 22:36:41 +0200

    Decompile paired room dispatch helpers

---
+++ src/overlays/room_m231/func_801902A0.c
@@ -0,0 +1,5 @@
+#include "../room_lib/room_lib.h"
+
+extern void RoomLib_ResetAndSignalB_801909E0(RoomEnt *o);
+
+ROOMLIB_STATE_DISPATCH_VARIANT2(func_801902A0, RoomLib_ResetAndSignalB_801909E0)
---
+++ src/overlays/room_m384/func_8018F558.c
@@ -0,0 +1,3 @@
+int func_8018F558(void) {
+    return 0;
+}
