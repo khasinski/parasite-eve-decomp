1363e12f686207c9f3a0f815fef664dc25647886
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 13:32:41 +0200

    Decompile func_800CE2B4

--- src/main/engine/func_800CE2B4.c
+++ src/main/engine/func_800CE2B4.c
@@ -8,11 +8,23 @@ extern u16 D_800E280A;
 extern u16 D_800E280C;
 
 int func_800CE2B4(void *arg0, void *arg1, u8 *anim) {
-    *(u16 *)(anim + 0x6) = D_800E2808 + (rand() % 80) - 0x28;
-    *(u16 *)(anim + 0x8) = D_800E280A + (rand() % 80) - 0x28;
+    register int base asm("$5");
+    register int r asm("$2");
+
+    r = rand();
+    base = D_800E2808 - 0x28;
+    *(u16 *)(anim + 0x6) = base + (r % 80);
+
+    r = rand();
+    base = D_800E280A - 0x28;
+    *(u16 *)(anim + 0x8) = base + (r % 80);
+
+    r = rand();
+    base = D_800E280C;
 
     anim[1] = 0x7F;
     *(u16 *)(anim + 0x4) = 0;
     anim[3] = 0;
-    *(u16 *)(anim + 0xA) = D_800E280C + (rand() % 80) - 0x28;
+    base -= 0x28;
+    *(u16 *)(anim + 0xA) = base + (r % 80);
 }
