465f46860e33fa0f63ae1c9f15cc7821cfd72520
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 21:19:31 +0200

    Decompile room state dispatchers

---
+++ src/overlays/room_m318/func_8018F578.c
@@ -0,0 +1,5 @@
+#include "../room_lib/room_lib.h"
+
+extern void func_8018FF10(RoomEnt *o);
+
+ROOMLIB_STATE_DISPATCH_VARIANT2(func_8018F578, func_8018FF10)
---
+++ src/overlays/room_m383/func_80193C94.c
@@ -0,0 +1,5 @@
+#include "../room_lib/room_lib.h"
+
+extern void func_8019462C(RoomEnt *o);
+
+ROOMLIB_STATE_DISPATCH_VARIANT2(func_80193C94, func_8019462C)
