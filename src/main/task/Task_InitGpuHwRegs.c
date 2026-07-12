93dde808491af10a1123cb6876e1d3b3d00eb08d
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-12 14:21:19 +0200

    Remove asm body from Task_InitGpuHwRegs

--- src/main/task/Task_InitGpuHwRegs.c
+++ src/main/task/Task_InitGpuHwRegs.c
@@ -1,6 +1,7 @@
 /* MASPSX_FLAGS: --store-return-delay */
 
 void Task_InitGpuHwRegs(void) {
+    register unsigned int zero asm("$0");
     register volatile int *ptr asm("$8");
     register unsigned int a asm("$11");
     register unsigned int b asm("$12");
@@ -10,12 +11,12 @@ void Task_InitGpuHwRegs(void) {
 
     ptr = (volatile int *)0x80070E0C;
     asm volatile("" : "=r"(ptr) : "0"(ptr));
-    asm volatile("ori %0, $0, 0x1" : "=r"(a));
+    a = zero | 1;
     ptr[0x10] = a;
-    asm volatile("ori %0, $0, 0x2" : "=r"(a));
+    a = zero | 2;
     ptr[0xF] = a;
     asm volatile("" : : "r"(ptr), "r"(a));
-    asm volatile("ori %0, $0, 0xE" : "=r"(count));
+    count = zero | 0xE;
 loop:
     a = ptr[0x10];
     b = ptr[0xF];
@@ -29,8 +30,8 @@ loop:
 
     ptr1 = (volatile int *)0x80070E04;
     ptr2 = (volatile int *)0x80070E08;
-    asm volatile("ori %0, $0, 0x40" : "=r"(a));
-    asm volatile("ori %0, $0, 0x10" : "=r"(b));
+    a = zero | 0x40;
+    b = zero | 0x10;
     *ptr1 = a;
     *ptr2 = b;
 }
