003191bc99dd467bd3369b22d2827c93030e510a
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-13 04:30:15 +0200

    Decompile more room handler F variants

---
+++ src/overlays/room_m163/func_80192358.c
@@ -0,0 +1,6 @@
+#include "../room_lib/room_lib.h"
+
+extern RoomLink *D_8009D254;
+extern int RoomLib_Set4ClearSignal_801924D8(RoomEnt *o);
+
+ROOMLIB_HANDLER_F_VARIANT(func_80192358, RoomLib_Set4ClearSignal_801924D8, D_8009D254)
---
+++ src/overlays/room_m358/func_801923A4.c
@@ -0,0 +1,6 @@
+#include "../room_lib/room_lib.h"
+
+extern RoomLink *D_8009D254;
+extern int RoomLib_Set4ClearSignal_80192524(RoomEnt *o);
+
+ROOMLIB_HANDLER_F_VARIANT(func_801923A4, RoomLib_Set4ClearSignal_80192524, D_8009D254)
