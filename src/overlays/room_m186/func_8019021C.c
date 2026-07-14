5186306dba99fd8ae7224039938f49d7469315ef
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-14 21:06:58 +0200

    Decompile room event advance handlers

---
+++ src/overlays/room_m186/func_8019021C.c
@@ -0,0 +1,7 @@
+int func_800C2B68(void);
+
+void func_8019021C(void *arg0, char *event) {
+    if (func_800C2B68() == 1) {
+        event[1] = 2;
+    }
+}
---
+++ src/overlays/room_m187/func_8018F3A8.c
@@ -0,0 +1,7 @@
+int func_800C2B68(void);
+
+void func_8018F3A8(void *arg0, char *event) {
+    if (func_800C2B68() == 1) {
+        event[1] = 2;
+    }
+}
