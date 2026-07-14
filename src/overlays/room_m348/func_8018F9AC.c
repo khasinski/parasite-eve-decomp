00fb6bb24de064d693b5503783d1a4d4537f61cd
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-15 01:27:57 +0200

    Decompile room m348 effect callbacks

---
+++ src/overlays/room_m348/func_8018F9AC.c
@@ -0,0 +1,27 @@
+typedef int s32;
+typedef short s16;
+typedef unsigned short u16;
+typedef unsigned char u8;
+
+void func_8018F9AC(s32 arg0, char *arg1, char *arg2) {
+    char *obj = arg2;
+    u16 value;
+
+    if (*(s16 *)(arg2 + 0x14) == 1) {
+        *(u16 *)(arg2 + 0x10) += 0xC8;
+    }
+
+    if (*(s16 *)(arg2 + 0x14) == 0) {
+        *(u16 *)(arg2 + 0x10) -= 0xC8;
+    }
+
+    value = *(u16 *)(obj + 0x12) - 0x10;
+    *(u16 *)(obj + 0x12) = value;
+    if ((s16)value < 0) {
+        *(u16 *)(obj + 0x12) = 0;
+    }
+
+    if (*(s16 *)(arg1 + 2) >= 0x15) {
+        arg1[1] = 2;
+    }
+}
