d7875c39ff83d7048ce2cb8a352057667f65fe2b
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-14 23:01:32 +0200

    Decompile room callback and position init helpers

---
+++ src/overlays/room_m146/func_8018F944.c
@@ -0,0 +1,17 @@
+#include "../room_lib/room_lib.h"
+
+void func_8018F944(void *unused0, void *unused1, char *state) {
+    char *obj = (char *)func_800C2B50();
+    register int tmp asm("$3");
+    register int cst asm("$2");
+
+    tmp = RW32(obj, 0x18);
+    RW16(state, 0) = tmp;
+    tmp = RW32(obj, 0x1C);
+    RW16(state, 2) = tmp;
+    tmp = RW32(obj, 0x20);
+    cst = 0x400;
+    RW16(state, 0xA) = 0;
+    RW16(state, 8) = cst;
+    RW16(state, 4) = tmp;
+}
