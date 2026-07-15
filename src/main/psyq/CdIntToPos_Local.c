739b2136b4832f804850d1983c02496c0637f495
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-15 11:02:38 +0200

    Decompile more libcd cd4 helpers

---
+++ src/main/psyq/CdIntToPos_Local.c
@@ -0,0 +1,13 @@
+#include "pe1/psyq_cd.h"
+
+static inline int ENCODE_BCD(int n) {
+    return ((n / 10) << 4) + (n % 10);
+}
+
+CdlLOC *CdIntToPos_Local(int i, CdlLOC *p) {
+    i += 150;
+    p->sector = ENCODE_BCD(i % 75);
+    p->second = ENCODE_BCD(i / 75 % 60);
+    p->minute = ENCODE_BCD(i / 75 / 60);
+    return p;
+}
---
+++ src/main/psyq/func_8007A88C.c
@@ -0,0 +1,6 @@
+void func_8007B964(void *arg0);
+
+int func_8007A88C(void *arg0) {
+    func_8007B964(arg0);
+    return 1;
+}
