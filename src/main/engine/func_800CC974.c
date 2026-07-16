9f14f4d006d79984ac542d1c2870fb5dea8f05b0
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-16 04:38:36 +0200

    Decompile engine animation timer drift step

--- src/main/engine/func_800CC974.c
+++ src/main/engine/func_800CC974.c
@@ -6,8 +6,8 @@ int func_800CC974(void *arg0, u8 *state, u8 *anim) {
     register u8 *state_t2 asm("$10");
     register u8 *timer_t0 asm("$8");
     register u8 *entry_a3 asm("$7");
-    int dx;
-    int dy;
+    register int dx asm("$3");
+    register int dy asm("$4");
     register int dz asm("$5");
     int frame;
 
@@ -20,16 +20,29 @@ int func_800CC974(void *arg0, u8 *state, u8 *anim) {
         asm volatile("" : "=r"(timer_t0) : "0"(timer_t0));
         entry_a3 = anim;
         do {
+            register int pos asm("$2");
+            register int angle asm("$3");
+
             i++;
             dx = *(short *)(entry_a3 + 0xA6) >> 9;
+            pos = *(u16 *)(entry_a3 + 0x26);
             dy = *(short *)(entry_a3 + 0xA8) >> 9;
             dz = *(short *)(entry_a3 + 0xAA) >> 9;
-            *(u16 *)(entry_a3 + 0x26) = *(u16 *)(entry_a3 + 0x26) + dx;
-            *(u16 *)(entry_a3 + 0x28) = *(u16 *)(entry_a3 + 0x28) + dy;
-            *(u16 *)(entry_a3 + 0xA8) = *(u16 *)(entry_a3 + 0xA8) + 0xB4;
-            *(u16 *)(entry_a3 + 0x2A) = *(u16 *)(entry_a3 + 0x2A) + dz;
-            *(u16 *)(timer_t0 + 0x6) = *(u16 *)(timer_t0 + 0x6) + 0x18;
+            pos += dx;
+            *(u16 *)(entry_a3 + 0x26) = pos;
+            pos = *(u16 *)(entry_a3 + 0x28);
+            angle = *(u16 *)(entry_a3 + 0xA8);
+            pos += dy;
+            *(u16 *)(entry_a3 + 0x28) = pos;
+            pos = *(u16 *)(entry_a3 + 0x2A);
+            angle += 0xB4;
+            *(u16 *)(entry_a3 + 0xA8) = angle;
+            pos += dz;
+            *(u16 *)(entry_a3 + 0x2A) = pos;
+            pos = *(u16 *)(timer_t0 + 0x6);
             entry_a3 += 8;
+            pos += 0x18;
+            *(u16 *)(timer_t0 + 0x6) = pos;
             timer_t0 += 2;
         } while (i < (signed char)anim[2]);
     }
