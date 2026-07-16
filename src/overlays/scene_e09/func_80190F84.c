41059cabf1c7f8f49ca788d8d6959ee969c27408
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-16 13:14:29 +0200

    Decompile scene_e09 scene_e10 dispatchers

---
+++ src/overlays/scene_e09/func_80190F84.c
@@ -0,0 +1,5 @@
+#include "../room_lib/room_lib.h"
+
+extern void func_80191914(RoomEnt *o);
+
+ROOMLIB_STATE_DISPATCH_VARIANT2_SHARED_DELAY(func_80190F84, func_80191914, 0x080643FC)
---
+++ src/overlays/scene_e10/func_80191CC4.c
@@ -0,0 +1,5 @@
+#include "../room_lib/room_lib.h"
+
+extern void func_801923FC(RoomEnt *o);
+
+ROOMLIB_STATE_DISPATCH_VARIANT2_SHARED_DELAY(func_80191CC4, func_801923FC, 0x0806474C)
