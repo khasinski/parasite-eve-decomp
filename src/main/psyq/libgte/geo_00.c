64917ca916f8bcda2a4ea29c8edd72d4c052eeee
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-16 09:29:26 +0200

    Decompile libgte sine helpers

--- src/main/psyq/libgte/geo_00.c
+++ src/main/psyq/libgte/geo_00.c
@@ -1,15 +1,5 @@
-#define LOAD_SIN_TABLE(offset, tableOffset)                                      \
-    ({                                                                           \
-        register int value asm("$2") = (offset);                                 \
-        asm volatile(".set noat\n"                                               \
-                     "lui $at,0x8009\n"                                         \
-                     "addu $at,$at,%0\n"                                        \
-                     "lh %0," tableOffset "($at)\n"                             \
-                     ".set at"                                                   \
-                     : "=r"(value)                                               \
-                     : "0"(value));                                              \
-        value;                                                                   \
-    })
+extern short D_8009489C[];
+extern short D_8009589C[];
 
 int sin_1(int angle);
 
@@ -26,18 +16,17 @@ int sin_1(int angle) {
 
     if (angle < 0x801) {
         if (angle < 0x401) {
-            return LOAD_SIN_TABLE(angle << 1, "0x589C");
+            return D_8009589C[angle];
         }
 
-        return LOAD_SIN_TABLE((0x800 - angle) << 1, "0x589C");
+        return D_8009589C[0x800 - angle];
     }
 
     if (angle < 0xC01) {
-        value = LOAD_SIN_TABLE(angle << 1, "0x489C");
-        asm volatile("nop");
+        value = D_8009489C[angle];
         return -value;
     }
 
-    value = LOAD_SIN_TABLE((0x1000 - angle) << 1, "0x589C");
+    value = D_8009589C[0x1000 - angle];
     return -value;
 }
