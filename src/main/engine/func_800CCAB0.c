68aa3525994316e5dedc89a69ed85b03794d8cb8
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-16 04:31:52 +0200

    Decompile engine animation drift step

--- src/main/engine/func_800CCAB0.c
+++ src/main/engine/func_800CCAB0.c
@@ -5,8 +5,8 @@ int func_800CCAB0(void *arg0, u8 *state, u8 *anim) {
     int i;
     register u8 *state_t1 asm("$9");
     register u8 *entry_a3 asm("$7");
-    int dx;
-    int dy;
+    register int dx asm("$3");
+    register int dy asm("$4");
     register int dz asm("$5");
     int frame;
 
@@ -17,14 +17,25 @@ int func_800CCAB0(void *arg0, u8 *state, u8 *anim) {
     if (*(short *)(anim + 0x4) > 0) {
         entry_a3 = anim;
         do {
+            register int pos asm("$2");
+            register int angle asm("$3");
+
             i++;
             dx = *(short *)(entry_a3 + 0x68) >> 8;
+            pos = *(u16 *)(entry_a3 + 0x8);
             dy = *(short *)(entry_a3 + 0x88) >> 8;
             dz = *(short *)(entry_a3 + 0xA8) >> 8;
-            *(u16 *)(entry_a3 + 0x8) = *(u16 *)(entry_a3 + 0x8) + dx;
-            *(u16 *)(entry_a3 + 0x28) = *(u16 *)(entry_a3 + 0x28) + dy;
-            *(u16 *)(entry_a3 + 0x88) = *(u16 *)(entry_a3 + 0x88) + 0xB4;
-            *(u16 *)(entry_a3 + 0x48) = *(u16 *)(entry_a3 + 0x48) + dz;
+            pos += dx;
+            *(u16 *)(entry_a3 + 0x8) = pos;
+            pos = *(u16 *)(entry_a3 + 0x28);
+            angle = *(u16 *)(entry_a3 + 0x88);
+            pos += dy;
+            *(u16 *)(entry_a3 + 0x28) = pos;
+            pos = *(u16 *)(entry_a3 + 0x48);
+            angle += 0xB4;
+            *(u16 *)(entry_a3 + 0x88) = angle;
+            pos += dz;
+            *(u16 *)(entry_a3 + 0x48) = pos;
             entry_a3 += 2;
         } while (i < *(short *)(anim + 0x4));
     }
