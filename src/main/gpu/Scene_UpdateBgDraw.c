8e4ab3b51b980f6987a7d239acf746a99a6befbc
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-16 05:49:04 +0200

    Decompile scene background draw update

--- src/main/gpu/Scene_UpdateBgDraw.c
+++ src/main/gpu/Scene_UpdateBgDraw.c
@@ -9,7 +9,8 @@ extern struct { char _[16]; } D_800B0CD8_o __asm__("D_800B0CD8");
 extern struct { char _[16]; } D_8009D254_o __asm__("D_8009D254");
 extern struct { char _[16]; } D_8009D1A0_o __asm__("D_8009D1A0");
 extern struct { char _[16]; } D_8009D2E8_o __asm__("D_8009D2E8");
-extern struct { char _[16]; } D_800B0D10_o __asm__("D_800B0D10");
+extern struct { char _[16]; } D_800B0D10_r __asm__("D_800B0D10");
+extern struct { char _[16]; } D_800B0D10_w __asm__("D_800B0D10");
 extern struct { char _[16]; } D_800B0D14_o __asm__("D_800B0D14");
 extern struct { char _[16]; } D_800B0D16_o __asm__("D_800B0D16");
 extern struct { char _[16]; } D_800B89F8_o __asm__("D_800B89F8");
@@ -18,7 +19,8 @@ extern struct { char _[16]; } D_800B89F8_o __asm__("D_800B89F8");
 #define D_8009D254 (*(u8 **)&D_8009D254_o)
 #define D_8009D1A0 (*(u32 *)&D_8009D1A0_o)
 #define D_8009D2E8 (*(u32 *)&D_8009D2E8_o)
-#define D_800B0D10 (*(u8 **)&D_800B0D10_o)
+#define D_800B0D10_READ (*(u8 **)&D_800B0D10_r)
+#define D_800B0D10_WRITE (*(u8 **)&D_800B0D10_w)
 #define D_800B0D14 (*(u16 *)&D_800B0D14_o)
 #define D_800B0D16 (*(u16 *)&D_800B0D16_o)
 #define D_800B0CEC ((u8 *)&D_800B0CD8_o + 0x14)
@@ -32,9 +34,9 @@ void Render_ResetGteScreenOffset(void);
 
 int Scene_UpdateBgDraw(void) {
     u8 *entity;
-    u8 *bg_actor;
+    register u8 *gameState asm("$16") = (u8 *)&D_800B0CD8_o;
 
-    if (D_800B0CD8 & 0xC0000) {
+    if (*(u32 *)gameState & 0xC0000) {
         return 0;
     }
 
@@ -51,19 +53,22 @@ int Scene_UpdateBgDraw(void) {
         return 0;
     }
 
-    if (D_800B0D10 == 0) {
-        D_800B0D10 = entity + 0x1B4;
+    if (D_800B0D10_READ == 0) {
+        register u8 *bgInit asm("$2");
+
+        bgInit = entity + 0x1B4;
+        D_800B0D10_WRITE = bgInit;
         D_800B0D14 = 3;
         D_800B0D16 = 0x12;
     }
 
-    bg_actor = D_800B0CEC;
+    gameState += 0x14;
     Render_SetGteScreenOffset();
-    Render_TransformVertices(bg_actor);
-    Render_TransformMorphVertices(bg_actor, D_800B89F8);
+    Render_TransformVertices(gameState);
+    Render_TransformMorphVertices(gameState, D_800B89F8);
 
     if ((D_8009D1A0 & 2) || D_8009D254[0x252] != 0) {
-        Render_DrawEntity(bg_actor, D_800B89F8);
+        Render_DrawEntity(gameState, D_800B89F8);
     }
 
     Render_ResetGteScreenOffset();
