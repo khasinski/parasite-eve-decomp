02e1b6db16e672862e50a6d0e323ecef6d75b2f3
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-16 03:11:40 +0200

    Match engine effect init helper

--- src/main/engine/func_800CBCA4.c
+++ src/main/engine/func_800CBCA4.c
@@ -1,136 +1,175 @@
-typedef unsigned char u8;
 typedef signed short s16;
+typedef unsigned char u8;
+typedef unsigned int u32;
 
-void **FieldEng_GetSlot(void);
+#define S16(base, off) (*(s16 *)((u8 *)(base) + (off)))
+#define PTR(base, off) (*(u8 **)((u8 *)(base) + (off)))
 
-extern int D_800E0D6C;
-extern s16 D_800E2290;
-extern s16 D_800E2292;
-extern s16 D_800E2294;
-extern void *D_800F3470;
+extern u32 *FieldEng_GetSlot(int slot);
 
-extern u8 D_800F3454;
-extern u8 D_800F3455;
-extern s16 D_800F3456;
-extern s16 D_800F3438;
-extern s16 D_800F343A;
-extern s16 D_800F343C;
+extern u8 D_800E0D6C[];
+extern volatile s16 D_800E2290;
+extern volatile s16 D_800E2292;
+extern volatile s16 D_800E2294;
+extern volatile u8 D_800E2284;
+extern volatile u8 D_800E2285;
+extern volatile s16 D_800E2286;
+extern volatile s16 D_800E2268;
+extern volatile s16 D_800E226A;
+extern volatile s16 D_800E226C;
+extern volatile s16 D_800E2820;
+extern volatile s16 D_800E2822;
+extern volatile s16 D_800E2824;
+extern volatile u32 D_800E2828;
+extern volatile u32 D_800E282C;
+extern volatile u32 D_800E2830;
+extern volatile u8 D_800E2838;
+extern volatile u8 D_800E2839;
+extern volatile u8 D_800E283A;
+extern volatile u8 D_800E283C;
+extern volatile u8 D_800E283D;
+extern volatile s16 D_800E283E;
 
-extern u8 D_800E2284;
-extern u8 D_800E2285;
-extern s16 D_800E2286;
-extern s16 D_800E2268;
-extern s16 D_800E226A;
-extern s16 D_800E226C;
+extern volatile u8 *D_800F3470;
+extern volatile u8 D_800F3454;
+extern volatile u8 D_800F3455;
+extern volatile s16 D_800F3456;
+extern volatile s16 D_800F3438;
+extern volatile s16 D_800F343A;
+extern volatile s16 D_800F343C;
+extern volatile s16 D_800F32E8;
+extern volatile s16 D_800F32EA;
+extern volatile s16 D_800F32EC;
+extern volatile u8 D_800F3300;
+extern volatile u8 D_800F3301;
+extern volatile u8 D_800F3302;
+extern volatile u8 D_800F3304;
+extern volatile u8 D_800F3305;
+extern volatile s16 D_800F3306;
+extern volatile s16 D_800F3388;
+extern volatile s16 D_800F338A;
+extern volatile s16 D_800F338C;
+extern volatile u8 D_800F33A4;
+extern volatile u8 D_800F33A5;
+extern volatile s16 D_800F33A6;
+extern volatile u8 D_800F335C;
+extern volatile u8 D_800F335D;
+extern volatile s16 D_800F335E;
+extern volatile u8 D_800F33A0;
+extern volatile u8 D_800F33A1;
+extern volatile u8 D_800F33A2;
+extern volatile s16 D_800F3340;
+extern volatile s16 D_800F3342;
+extern volatile s16 D_800F3344;
+extern volatile u8 D_800F3358;
+extern volatile u8 D_800F3359;
+extern volatile u8 D_800F335A;
 
-extern u8 D_800F3300;
-extern u8 D_800F3301;
-extern u8 D_800F3302;
-extern u8 D_800F3304;
-extern u8 D_800F3305;
-extern s16 D_800F3306;
-extern s16 D_800F32E8;
-extern s16 D_800F32EA;
-extern s16 D_800F32EC;
-extern s16 D_800F3388;
-extern s16 D_800F338A;
-extern s16 D_800F338C;
+int func_800CBCA4(u8 *arg0) {
+    register u8 *obj asm("$3");
+    register int tmp asm("$2");
+    register int y_scale asm("$5");
+    register int color asm("$3");
+    register int z asm("$6");
+    register int orange asm("$4");
+    register int y_off asm("$4");
+    u32 *slot;
 
-extern u8 D_800F33A0;
-extern u8 D_800F33A1;
-extern u8 D_800F33A2;
-extern u8 D_800F33A4;
-extern u8 D_800F33A5;
-extern s16 D_800F33A6;
+    slot = FieldEng_GetSlot((int)arg0);
+    *slot = (u32)D_800E0D6C;
+    obj = PTR(arg0, 8);
 
-extern int D_800E2828;
-extern int D_800E282C;
-extern int D_800E2830;
-extern u8 D_800E2838;
-extern u8 D_800E2839;
-extern u8 D_800E283A;
-extern u8 D_800E283C;
-extern u8 D_800E283D;
-extern s16 D_800E2820;
-extern s16 D_800E2822;
-extern s16 D_800E2824;
-extern s16 D_800E283E;
+    tmp = S16(obj, 0x2A);
+    y_off = -0x64;
+    D_800E2290 = tmp;
+    tmp = S16(obj, 0x2E);
+    y_scale = 0x400;
+    D_800F3470 = obj;
+    D_800E2292 = tmp;
 
-extern u8 D_800F3358;
-extern u8 D_800F3359;
-extern u8 D_800F335A;
-extern u8 D_800F335C;
-extern u8 D_800F335D;
-extern s16 D_800F335E;
-extern s16 D_800F3340;
-extern s16 D_800F3342;
-extern s16 D_800F3344;
+    tmp = 0;
+    asm volatile("" : "=r"(tmp) : "0"(tmp) : "memory");
+    z = S16(obj, 0x32);
+    color = 0x68;
+    D_800F3454 = color;
+    D_800E2284 = color;
+    D_800F3304 = color;
 
-void func_800CBCA4(void *obj) {
-    char *data;
-    int neg100 = -0x64;
-    int wide = 0x400;
-    int full;
+    color = 0xA0;
+    D_800F3300 = color;
+    D_800F3301 = color;
+    D_800F3302 = color;
 
-    *FieldEng_GetSlot() = &D_800E0D6C;
+    color = 0x46;
+    D_800F3456 = y_off;
+    D_800F3306 = y_off;
 
-    data = *(char **)((char *)obj + 8);
-    D_800E2290 = *(s16 *)(data + 0x2A);
-    D_800F3470 = data;
-    D_800E2292 = *(s16 *)(data + 0x2E);
-
-    D_800F3454 = 0x68;
-    D_800E2284 = 0x68;
-    D_800F3304 = 0x68;
-    D_800F3300 = 0xA0;
-    D_800F3301 = 0xA0;
-    D_800F3302 = 0xA0;
-    D_800F3456 = neg100;
-    D_800F3306 = neg100;
+    orange = 0x80;
     D_800F3438 = 0;
     D_800F343A = 0;
     D_800F343C = 0;
     D_800F3455 = 0;
-    D_800E2268 = wide;
+
+    D_800E2268 = y_scale;
     D_800E226A = 0;
     D_800E226C = 0;
     D_800E2285 = 0;
     D_800E2286 = 0;
-    D_800F32E8 = wide;
+
+    D_800F32E8 = y_scale;
     D_800F32EA = 0;
     D_800F32EC = 0;
     D_800F3305 = 0;
-    D_800F3388 = wide;
+
+    D_800F3388 = y_scale;
     D_800F338A = 0;
     D_800F338C = 0;
-    D_800E2294 = *(s16 *)(data + 0x32);
-    D_800F33A4 = 0x46;
-    D_800F33A5 = 0x30;
-    D_800F33A6 = -0x50;
-    D_800E2828 = 0x1A8;
-    D_800E282C = 0x1A8;
-    D_800E2830 = 0x1A8;
-    D_800E283C = 0xBC;
-    D_800E283D = 8;
-    D_800F335C = 0x42;
-    D_800F335D = 0x20;
 
-    full = 0x80;
-    D_800F33A0 = full;
-    D_800F33A1 = full;
-    D_800F33A2 = full;
+    D_800E2294 = z;
+    D_800F33A4 = color;
+
+    color = 0x30;
+    D_800F33A5 = color;
+
+    color = -0x50;
+    D_800F33A6 = color;
+
+    color = 0x1A8;
+    D_800E2828 = color;
+    D_800E282C = color;
+    D_800E2830 = color;
+
+    color = 0xBC;
+    D_800E283C = color;
+
+    color = 8;
+    D_800E283D = color;
+
+    color = 0x42;
+    D_800F335C = color;
+
+    color = 0x20;
+    D_800F335D = color;
+
+    color = 0x32;
+    D_800F33A0 = orange;
+    D_800F33A1 = orange;
+    D_800F33A2 = orange;
+
     D_800E2820 = 0;
     D_800E2822 = 0;
     D_800E2824 = 0;
     D_800E283E = 0;
-    D_800E2838 = full;
-    D_800E2839 = full;
-    D_800E283A = full;
-    D_800F3340 = wide;
+    D_800E2838 = orange;
+    D_800E2839 = orange;
+    D_800E283A = orange;
+
+    D_800F3340 = y_scale;
     D_800F3342 = 0;
     D_800F3344 = 0;
-    D_800F335E = 0x32;
-    D_800F3358 = full;
-    D_800F3359 = full;
-    D_800F335A = full;
+    D_800F335E = color;
+    D_800F3358 = orange;
+    D_800F3359 = orange;
+    D_800F335A = orange;
+    return tmp;
 }
