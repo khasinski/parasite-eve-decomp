c9bcf9e6cf699cea246a680fa5dc350374a66fde
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-17 19:50:03 +0200

    Decompile room entry copy helpers

---
+++ src/overlays/room_m153/func_80190044.c
@@ -0,0 +1,23 @@
+#include "../room_lib/room_lib.h"
+
+typedef struct {
+    short x;
+    short y;
+    short z;
+    short pad;
+} Entry;
+
+void func_80190044(void *unused0, void *unused1, char *state) {
+    char *obj = (char *)func_800C2B50();
+    unsigned int i;
+    Entry *entries = (Entry *)state;
+
+    for (i = 0; i < 0x10; i++) {
+        entries[i].x = RW32(obj, 0x18);
+        entries[i].y = RW32(obj, 0x1C);
+        entries[i].z = RW32(obj, 0x20);
+    }
+
+    RW16(state, 0x82) = 0x80;
+    RW16(state, 0x80) = 0x800;
+}
