5e7cf2564d68983f7f487275b676bd3c757d5b0c
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-17 22:52:42 +0200

    Decompile room tick main helpers

---
+++ src/overlays/room_m063/RoomLib_TickMain.c
@@ -0,0 +1,19 @@
+int RoomLib_TickMain(char *ctx) {
+    char *node;
+
+    ctx[0] = 4;
+    ctx[3] = 0;
+
+    *(int *)(*(char **)(ctx + 8) + 0x98) &= 0xFFFEFFFF;
+    *(unsigned short *)(*(char **)(ctx + 8) + 0x250) &= 0xFBFF;
+
+    node = *(char **)(*(char **)(ctx + 8) + 0x18C);
+    if (node != 0) {
+        node = *(char **)node;
+        if (node != 0) {
+            (*(char **)(node + 0x18))[0] = 4;
+        }
+    }
+
+    return 0;
+}
