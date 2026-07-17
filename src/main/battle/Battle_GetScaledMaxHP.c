fe2c7566dcaecedeed6a89a460df597fc3ecb676
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-17 09:17:41 +0200

    Remove asm alias from scaled HP helper

--- src/main/battle/Battle_GetScaledMaxHP.c
+++ src/main/battle/Battle_GetScaledMaxHP.c
@@ -1,4 +1,4 @@
-extern int g_StatScaleBase __asm__("D_800A1B30");
+extern int D_800A1B30;
 
 unsigned short *Aya_LookupLevelStats(int level);
 
@@ -6,5 +6,5 @@ int Battle_GetScaledMaxHP(int level) {
     unsigned short maxHp;
 
     maxHp = *Aya_LookupLevelStats(level);
-    return ((g_StatScaleBase + 0x14) * maxHp) / 20;
+    return ((D_800A1B30 + 0x14) * maxHp) / 20;
 }
