b1342de29e04b02869079a5f3b3d866a4fd7d12d
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-25 21:36:05 +0200

    Add missing scene_e08_header source
    The committed scene_e08 config referenced scene_e08_header as a c segment,
    but the source file was never tracked. Committing it so the overlay builds
    from a clean checkout.

---
+++ src/overlays/scene_e08/scene_e08_header.c
@@ -0,0 +1,98 @@
+unsigned int D_8018EFF0[] __attribute__((section(".text"))) = {
+    0x00050134,
+};
+
+unsigned int D_8018EFF4[] __attribute__((section(".text"))) = {
+    0x0004FE24, 0x00000010,
+};
+
+unsigned int D_8018EFFC[] __attribute__((section(".text"))) = {
+    0x00000000, 0x00000000,
+};
+
+unsigned int D_8018F004[] __attribute__((section(".text"))) = {
+    0x00000000,
+};
+
+unsigned int D_8018F008[] __attribute__((section(".text"))) = {
+    0x00000000, 0x00101010, 0x00000190, 0x00000190,
+};
+
+unsigned int D_8018F018[] __attribute__((section(".text"))) = {
+    0x00000190, 0x00000000,
+};
+
+unsigned int D_8018F020[] __attribute__((section(".text"))) = {
+    0x00000400, 0x00000000, 0x00000708, 0x00000708,
+};
+
+unsigned int D_8018F030[] __attribute__((section(".text"))) = {
+    0x00000708, 0x00000000, 0x00000960, 0x00000960,
+};
+
+unsigned int D_8018F040[] __attribute__((section(".text"))) = {
+    0x00000960, 0x00000000,
+};
+
+unsigned int D_8018F048[] __attribute__((section(".text"))) = {
+    0x00000000,
+};
+
+unsigned int D_8018F04C[] __attribute__((section(".text"))) = {
+    0x00000000,
+};
+
+unsigned int D_8018F050[] __attribute__((section(".text"))) = {
+    0x00808080, 0x00FFFFFF, 0x000004C8, 0x000004C8,
+};
+
+unsigned int D_8018F060[] __attribute__((section(".text"))) = {
+    0x000004C8,
+};
+
+unsigned int D_8018F064[] __attribute__((section(".text"))) = {
+    0x00000000,
+};
+
+unsigned int jtbl_8018F068[] __attribute__((section(".text"))) = {
+    0x00804020, 0x00802040, 0x80195470, 0x8019567C,
+    0x80195540, 0x80195548, 0x80195538, 0x8019567C,
+    0x801955A8, 0x8019567C, 0x8019567C, 0x8019567C,
+    0x80195580, 0x8019559C, 0x8019567C, 0x8019567C,
+    0x8019567C, 0x8019567C, 0x8019567C, 0x801954E0,
+    0x8019567C, 0x80195430, 0x801955C4, 0x801955D4,
+    0x80195650, 0x8019555C, 0x8019567C, 0x80195458,
+    0x80195514, 0x8019567C, 0x8019567C, 0x8019567C,
+};
+
+unsigned int jtbl_8018F0E8[] __attribute__((section(".text"))) = {
+    0x8019566C, 0x80195674, 0x801961B0, 0x801963BC,
+    0x801963BC, 0x801963BC, 0x80196254, 0x801963BC,
+    0x80196264, 0x801963BC, 0x801963BC, 0x801963BC,
+    0x80196324, 0x80196340, 0x801963BC, 0x801963BC,
+    0x801963BC, 0x80196274, 0x801963BC, 0x80196220,
+    0x801963BC, 0x8019617C, 0x801963BC, 0x80196280,
+    0x801962F0, 0x80196300, 0x8019634C, 0x80196198,
+    0x801963BC, 0x80196364, 0x801963BC, 0x801963BC,
+    0x801963BC, 0x801963B0,
+};
+
+unsigned int jtbl_8018F170[] __attribute__((section(".text"))) = {
+    0x801963B8, 0x00000000, 0x80196D2C, 0x80196E50,
+    0x80196C80, 0x80196E50, 0x80196C74, 0x80196E50,
+    0x80196CA0, 0x80196E50, 0x80196E50, 0x80196E50,
+    0x80196CD4, 0x80196CEC, 0x80196C88, 0x80196C90,
+    0x80196C98, 0x80196E50, 0x80196CF8, 0x80196D9C,
+    0x80196D00, 0x80196C40, 0x80196E50, 0x80196DD4,
+    0x80196E44, 0x80196CB0,
+};
+
+unsigned int jtbl_8018F1D8[] __attribute__((section(".text"))) = {
+    0x80196E50, 0x80196C5C, 0x8019778C, 0x80197968,
+    0x80197968, 0x80197968, 0x80197968, 0x80197968,
+    0x80197840, 0x80197968, 0x80197968, 0x80197968,
+    0x80197874, 0x8019789C, 0x80197968, 0x80197838,
+    0x80197968, 0x80197830, 0x801978B8, 0x801977FC,
+    0x801978C0, 0x80197758, 0x80197968, 0x801978EC,
+    0x8019795C, 0x80197850, 0x80197968, 0x80197774,
+};
