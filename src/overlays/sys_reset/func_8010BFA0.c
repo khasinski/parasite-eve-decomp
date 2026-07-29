e590defee7f410875566394a1304b7660c3770ba
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-30 01:24:55 +0200

    Decompile sys_reset DecDCTin

---
+++ src/overlays/sys_reset/func_8010BFA0.c
@@ -0,0 +1,21 @@
+typedef int s32;
+typedef unsigned int u32;
+typedef unsigned short u16;
+
+extern void func_8010C1EC(s32 *arg0, u32 arg1);
+
+void func_8010BFA0(s32 *arg0, s32 arg1) {
+    if (arg1 & 1) {
+        *arg0 &= ~0x08000000;
+    } else {
+        *arg0 |= 0x08000000;
+    }
+
+    if (arg1 & 2) {
+        *arg0 |= 0x02000000;
+    } else {
+        *arg0 &= ~0x02000000;
+    }
+
+    func_8010C1EC(arg0, *(u16 *)arg0);
+}
