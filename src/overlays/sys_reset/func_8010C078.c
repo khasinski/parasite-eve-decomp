c014a6801d462436d7d237c0b7c8213361e86e33
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-30 01:25:48 +0200

    Decompile sys_reset sync wrappers

---
+++ src/overlays/sys_reset/func_8010C078.c
@@ -0,0 +1,16 @@
+typedef int s32;
+typedef unsigned int u32;
+
+extern s32 func_8010C39C(void);
+extern s32 func_8010C430(void);
+
+s32 func_8010C078(s32 mode) {
+    s32 result;
+
+    if (mode == 0) {
+        result = func_8010C39C();
+    } else {
+        result = ((u32)func_8010C430() >> 24) & 1;
+    }
+    return result;
+}
