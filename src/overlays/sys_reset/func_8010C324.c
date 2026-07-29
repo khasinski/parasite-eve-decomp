82811110cd88d03a6cf0b2451f1f01a3bdbe7387
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-30 01:27:16 +0200

    Name sys_reset DecDCTout parameters

--- src/overlays/sys_reset/func_8010C324.c
+++ src/overlays/sys_reset/func_8010C324.c
@@ -1,5 +1,8 @@
-void func_8010C27C(void);
+typedef int s32;
+typedef unsigned int u32;
 
-void func_8010C324(void) {
-    func_8010C27C();
+extern void func_8010C27C(s32 *buffer, u32 size);
+
+void func_8010C324(s32 *buffer, u32 size) {
+    func_8010C27C(buffer, size);
 }
