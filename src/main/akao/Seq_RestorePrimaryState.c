fd3d1eb87e8c2ef240aeffb49bdf7e05d526e5ae
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-07-30 00:05:28 +0200

    Use AKAO voice bank in state restore

--- src/main/akao/Seq_RestorePrimaryState.c
+++ src/main/akao/Seq_RestorePrimaryState.c
@@ -4,33 +4,33 @@ typedef unsigned int u32;
 typedef unsigned short u16;
 
 extern char g_AkaoTrackStateBackup[];
-extern char g_AkaoVoiceStateBackup[];
-extern char g_AkaoVoiceStateTable[];
+extern AkaoVoiceBank g_AkaoVoiceStateBackup[];
+extern AkaoVoiceBank g_AkaoVoiceBanks[] __asm__("g_AkaoVoiceStateTable");
 extern char *g_AkaoCurTrack;
 
 void Util_CopyWords(unsigned int *src, unsigned int *dst, unsigned int size);
 
 void Seq_RestorePrimaryState(void) {
     u32 i;
-    char *ptr;
+    AkaoTrack *track;
     u32 flags;
     u16 value;
 
     if (*(u32 *)(g_AkaoCurTrack + 4) != 0) {
         Util_CopyWords(g_AkaoCurTrack, g_AkaoTrackStateBackup, 0x68);
-        Util_CopyWords(g_AkaoVoiceStateTable, g_AkaoVoiceStateBackup, 0x1AA0);
+        Util_CopyWords((u32 *)&g_AkaoVoiceBanks[0], (u32 *)&g_AkaoVoiceStateBackup[0], sizeof(AkaoVoiceBank));
 
         flags = *(u32 *)g_AkaoTrackStateBackup;
         if ((flags & 0x100) != 0) {
             i = 0;
-            ptr = g_AkaoVoiceStateBackup + 0x5A;
+            track = g_AkaoVoiceStateBackup[0].tracks;
             do {
-                value = *(u16 *)ptr;
+                value = track->note_pitch;
                 if (value >= 0x50) {
-                    *(u16 *)ptr = value - 0x30;
+                    track->note_pitch = value - 0x30;
                 }
                 i++;
-                ptr += sizeof(AkaoTrack);
+                track++;
             } while (i < 24);
         }
     }
