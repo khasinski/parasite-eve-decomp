4f9e3ae1f816e5842e0f693ee6382aabd313456c
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-08-11 22:15:13 +0200

    Unify remaining state dispatch variants

--- src/overlays/room_m188/func_8018F534.c
+++ src/overlays/room_m188/func_8018F534.c
@@ -1,20 +1,5 @@
 #include "../room_lib/room_lib.h"
 
-extern int func_800DFB78(void);
 extern void func_8018FECC(RoomEnt *o);
 
-int func_8018F534(RoomEnt *o) {
-    switch (func_800DFB78()) {
-    case 0:
-        if (o->link->variant < 2) {
-            return 0;
-        }
-        ((void (*)(RoomEnt *))o->sub.cb)(o);
-        return 0;
-    case 1:
-        func_8018FECC(o);
-    case 2:
-        return 0;
-    }
-    return 0;
-}
+ROOMLIB_STATE_DISPATCH_VARIANT2(func_8018F534, func_8018FECC)
--- src/overlays/room_m410/func_8018F50C.c
+++ src/overlays/room_m410/func_8018F50C.c
@@ -2,18 +2,4 @@
 
 extern void func_8018FEA4(RoomEnt *o);
 
-int func_8018F50C(RoomEnt *o) {
-    switch (func_800DFB78()) {
-    case 0:
-        if (o->link->variant < 2) {
-            return 0;
-        }
-        ((void (*)(RoomEnt *))o->sub.cb)(o);
-        return 0;
-    case 1:
-        func_8018FEA4(o);
-    case 2:
-        return 0;
-    }
-    return 0;
-}
+ROOMLIB_STATE_DISPATCH_VARIANT2(func_8018F50C, func_8018FEA4)
