055d6991b8c44066554401331393dd6fc947fb87
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-08-12 12:03:40 +0200

    Decompile remaining room argument handlers

---
+++ src/overlays/room_m152/RoomLib_HandlerBArgs_80190AD0.c
@@ -0,0 +1,5 @@
+#include "../room_lib/room_lib.h"
+
+ROOMLIB_HANDLER_B_ARGS(RoomLib_HandlerBArgs_80190AD0,
+                       RoomLib_ArmWindowA_80190D48,
+                       func_8019114C)
---
+++ src/overlays/room_m152/RoomLib_HandlerCArgs_801915E8.c
@@ -0,0 +1,5 @@
+#include "../room_lib/room_lib.h"
+
+ROOMLIB_HANDLER_C_ARGS(RoomLib_HandlerCArgs_801915E8,
+                       RoomLib_ArmWindowB_80191860,
+                       func_80191D54)
