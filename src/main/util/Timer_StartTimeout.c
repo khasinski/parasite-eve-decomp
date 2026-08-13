ad2e03600634f2fb9c61762c46f172b77055bd69
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-08-13 12:43:30 +0200

    Match stream and timer state setters

--- src/main/util/Timer_StartTimeout.c
+++ src/main/util/Timer_StartTimeout.c
@@ -1,11 +1,24 @@
 #include "common.h"
 
-extern int g_TimerTimeoutLimit;
-extern int g_TimerTimeoutStart;
+typedef struct TimerLimitPage {
+    int limit;
+    char reserved04[0x2FD0];
+} TimerLimitPage;
 
-void Timer_StartTimeout(int arg0) {
+typedef struct TimerStartPage {
+    char reserved00[0x76D0];
+    int start;
+} TimerStartPage;
+
+register void *g_TimerWritePage asm("$1");
+
+void Timer_StartTimeout(int limit) {
     volatile u16 *timer = (volatile u16 *)0x1F801120;
+    int start;
 
-    g_TimerTimeoutLimit = arg0;
-    g_TimerTimeoutStart = *timer;
+    start = *timer;
+    g_TimerWritePage = (void *)0x800C0000;
+    ((TimerLimitPage *)g_TimerWritePage)[-1].limit = limit;
+    g_TimerWritePage = (void *)0x800A0000;
+    ((TimerStartPage *)g_TimerWritePage)->start = start;
 }
