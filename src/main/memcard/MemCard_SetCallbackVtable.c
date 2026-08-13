e8b5c91a658eb94c185331b0167998627b33431c
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-08-13 12:53:32 +0200

    Match hardware callback initializers

--- src/main/memcard/MemCard_SetCallbackVtable.c
+++ src/main/memcard/MemCard_SetCallbackVtable.c
@@ -1,14 +1,26 @@
 
+/* CC1_FLAGS: -fno-schedule-insns */
+
 extern void func_80084B78(void);
 extern void CardObj_IsTransferActive(void);
 extern void CardObj_HandleResponse(void);
 
+typedef struct MemCardCallbackDataPage {
+    void (*response_handler)(void);
+    char reserved04[0x48B8];
+} MemCardCallbackDataPage;
+
 extern void (*g_MemCardStateDispatchFn)(void);
 extern void (*g_MemCardIsTransferActiveFn)(void);
-extern void (*g_MemCardResponseHandler)(void);
+
+register MemCardCallbackDataPage *g_MemCardCallbackWritePage asm("$1");
 
 void MemCard_SetCallbackVtable(void) {
+    void (*response_handler)(void);
+
     g_MemCardStateDispatchFn = func_80084B78;
     g_MemCardIsTransferActiveFn = CardObj_IsTransferActive;
-    g_MemCardResponseHandler = CardObj_HandleResponse;
+    response_handler = CardObj_HandleResponse;
+    g_MemCardCallbackWritePage = (MemCardCallbackDataPage *)0x800A0000;
+    g_MemCardCallbackWritePage[-1].response_handler = response_handler;
 }
--- src/main/task/Task_InitGpuHwRegs.c
+++ src/main/task/Task_InitGpuHwRegs.c
@@ -1,4 +1,8 @@
 
+/* CC1_FLAGS: -fno-schedule-insns */
+
+register int *g_TaskGpuReg2 asm("$10");
+
 void Task_InitGpuHwRegs(void) {
     register unsigned int zero asm("$0");
     register volatile int *ptr asm("$8");
@@ -6,7 +10,6 @@ void Task_InitGpuHwRegs(void) {
     register unsigned int b asm("$12");
     register unsigned int count asm("$13");
     register volatile int *ptr1 asm("$9");
-    register volatile int *ptr2 asm("$10");
 
     ptr = (volatile int *)0x80070E0C;
     asm volatile("" : "=r"(ptr) : "0"(ptr));
@@ -16,21 +19,18 @@ void Task_InitGpuHwRegs(void) {
     ptr[0xF] = a;
     asm volatile("" : : "r"(ptr), "r"(a));
     count = zero | 0xE;
-loop:
-    a = ptr[0x10];
-    b = ptr[0xF];
-    ptr--;
-    a += b;
-    ptr[0xF] = a;
-    if (count != 0) {
-        count--;
-        goto loop;
-    }
+    do {
+        a = ptr[0x10];
+        b = ptr[0xF];
+        ptr--;
+        a += b;
+        ptr[0xF] = a;
+    } while (count != 0 && (count--, 1));
 
     ptr1 = (volatile int *)0x80070E04;
-    ptr2 = (volatile int *)0x80070E08;
+    g_TaskGpuReg2 = (int *)0x80070E08;
     a = zero | 0x40;
     b = zero | 0x10;
     *ptr1 = a;
-    *ptr2 = b;
+    *g_TaskGpuReg2 = b;
 }
