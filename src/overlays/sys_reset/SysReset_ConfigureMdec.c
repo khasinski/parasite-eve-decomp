0279195d25b4310755ad2d1b328e0f882851a96c
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-08-13 15:03:10 +0200

    Schedule MDEC control writes

--- src/overlays/sys_reset/SysReset_ConfigureMdec.c
+++ src/overlays/sys_reset/SysReset_ConfigureMdec.c
@@ -1,14 +1,13 @@
 #include "common.h"
 #include "pe1/sys_reset.h"
 
-
 void func_8010C0FC(int mode) {
     switch (mode) {
     case 0:
         *D_8010DB50 = 0x80000000;
         *D_8010DB24 = 0;
         *D_8010DB30 = 0;
-        *D_8010DB50 = 0x60000000;
+        *(u32 *)D_8010DB50 = 0x60000000;
         func_8010C1EC(D_8010DA0C, 0x20);
         func_8010C1EC(D_8010DA90, 0x20);
         break;
@@ -17,7 +16,7 @@ void func_8010C0FC(int mode) {
         *D_8010DB24 = 0;
         *D_8010DB30 = 0;
         (void)*D_8010DB30;
-        *D_8010DB50 = 0x60000000;
+        *(u32 *)D_8010DB50 = 0x60000000;
         break;
     default:
         func_80071A74(D_8010BCFC, mode);
