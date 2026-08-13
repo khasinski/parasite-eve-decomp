036c841bfd9e32c0b05d9d62d70bad28c571b52e
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-08-13 13:24:32 +0200

    Match CD disk-kind detection

--- src/main/psyq/libds/GD_disk_kind.c
+++ src/main/psyq/libds/GD_disk_kind.c
@@ -1,11 +1,17 @@
 #include "common.h"
 
-extern int D_800B28F8;
 extern char D_8001205C[];
 
 int CdRom_IsBusy(u8 *dst, int sector_size);
 int strncmp(char *s1, char *s2, int n);
 void DsReadBreak(void);
+
+typedef struct CdDiskKindPage {
+    char reserved00[0x28F8];
+    int disk_type;
+} CdDiskKindPage;
+
+register CdDiskKindPage *g_CdDiskKindPage asm("$1");
 
 void GD_disk_kind(u8 arg0) {
     u8 buffer[8];
@@ -22,6 +28,7 @@ void GD_disk_kind(u8 arg0) {
         disk_type = 2;
     }
 
-    D_800B28F8 = disk_type;
+    g_CdDiskKindPage = (CdDiskKindPage *)0x800B0000;
+    g_CdDiskKindPage->disk_type = disk_type;
     DsReadBreak();
 }
