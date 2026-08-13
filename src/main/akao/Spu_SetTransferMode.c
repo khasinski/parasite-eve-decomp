8a81af215f3b3d2625b6f2b2c19208f76dac3d35
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-08-13 13:54:48 +0200

    Match SPU transfer mode callback path

--- src/main/akao/Spu_SetTransferMode.c
+++ src/main/akao/Spu_SetTransferMode.c
@@ -2,11 +2,20 @@
 extern int _spu_IRQCallback;
 void _SpuCallback(int arg0);
 
+typedef struct SpuTransferModePage {
+    int callback;
+    char reserved04[0x4BC4];
+} SpuTransferModePage;
+
+register SpuTransferModePage *g_SpuTransferModePage asm("$1");
+
 int Spu_SetTransferMode(int arg0) {
     int old = _spu_IRQCallback;
 
     if (arg0 != old) {
-        _SpuCallback(_spu_IRQCallback = arg0);
+        g_SpuTransferModePage = (SpuTransferModePage *)0x800A0000;
+        g_SpuTransferModePage[-1].callback = arg0;
+        _SpuCallback(arg0);
     }
     return old;
 }
