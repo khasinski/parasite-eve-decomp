149a1a46ac46dd9980ad4e40fbe487515fa61a1b
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-08-13 17:04:01 +0200

    Match CD seek callback segment

--- src/main/cdrom/misc9.c
+++ src/main/cdrom/misc9.c
@@ -1,14 +1,18 @@
-
+/* CC1_FLAGS: -O1 */
 void CdRom_AsyncCallback(void);
 int DsSyncCallback();
 
 extern int g_DsReadBusy;
+register volatile int g_CdCallback asm("$4");
 
 int DsRead_IsBusy(void);
 
-void CdRom_SeekDoneCallback(unsigned char arg0) {
-    if (arg0 == 2) {
-        DsSyncCallback((void (*)(void))0x80081E70);
+void CdRom_SeekDoneCallback(void) {
+    g_CdCallback = (unsigned char)g_CdCallback;
+    if (g_CdCallback == 2) {
+        g_CdCallback = 0x80080000;
+        g_CdCallback += 0x1E70;
+        DsSyncCallback((void (*)(void))g_CdCallback);
     }
 }
 
