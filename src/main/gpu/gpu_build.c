3785c2f3219f98f15d2afbc22d82420924a93ce8
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-08-13 21:22:51 +0200

    Match GPU texture window builder

--- src/main/gpu/gpu_build.c
+++ src/main/gpu/gpu_build.c
@@ -9,6 +9,12 @@ typedef struct {
     s16 h;
 } TexWindow;
 
+typedef struct GpuTexWindowWork {
+    volatile int slots[4];
+} GpuTexWindowWork;
+
+register GpuTexWindowWork *g_GpuTexWindowWork asm("$29");
+
 unsigned int Gpu_BuildDrawOffsetCmd(unsigned int arg0, unsigned int arg1) {
     arg1 &= 0x7FF;
     arg1 <<= 11;
@@ -18,29 +24,33 @@ unsigned int Gpu_BuildDrawOffsetCmd(unsigned int arg0, unsigned int arg1) {
 }
 
 u32 Gpu_BuildTexWindowCmd(TexWindow *tw) {
-    volatile int slots[4];
     int x;
     int y;
     int w;
     int h_raw;
     int h;
     register u32 command asm("$4");
+    register u32 result asm("$2");
 
-    if (tw != 0) {
+    g_GpuTexWindowWork--;
+    if (tw == 0) {
+        result = 0;
+    } else {
         x = tw->x >> 3;
-        slots[0] = x;
+        g_GpuTexWindowWork->slots[0] = x;
         w = ((-tw->w) & 0xFF) >> 3;
-        slots[2] = w;
+        g_GpuTexWindowWork->slots[2] = w;
         y = tw->y >> 3;
-        slots[1] = y;
+        g_GpuTexWindowWork->slots[1] = y;
         y <<= 15;
         h_raw = tw->h;
         command = 0xE2000000;
         h = ((-h_raw) & 0xFF) >> 3;
-        slots[3] = h;
+        g_GpuTexWindowWork->slots[3] = h;
 
-        return y | ((x << 10) | command) | (h << 5) | w;
+        result = y | ((x << 10) | command) | (h << 5) | w;
     }
 
-    return 0;
+    g_GpuTexWindowWork++;
+    return result;
 }
