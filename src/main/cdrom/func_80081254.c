94723348fd3eda22eedcb74ce39d13b3e017a80c
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-08-13 11:17:51 +0200

    Match global value swap helpers

--- src/main/cdrom/func_80081254.c
+++ src/main/cdrom/func_80081254.c
@@ -1,10 +1,18 @@
 
 extern int D_8009B6D0;
 
+typedef struct CdCallbackDataPage {
+    int callback;
+    char reserved04[0x492C];
+} CdCallbackDataPage;
+
+register CdCallbackDataPage *g_CdCallbackWritePage asm("$1");
+
 int func_80081254(int callback) {
     int old;
 
     old = D_8009B6D0;
-    D_8009B6D0 = callback;
+    g_CdCallbackWritePage = (CdCallbackDataPage *)0x800A0000;
+    g_CdCallbackWritePage[-1].callback = callback;
     return old;
 }
--- src/main/gpu/SetVideoMode.c
+++ src/main/gpu/SetVideoMode.c
@@ -1,10 +1,18 @@
 
 extern int D_800956EC;
 
+typedef struct VideoModeDataPage {
+    char reserved00[0x56EC];
+    int video_mode;
+} VideoModeDataPage;
+
+register VideoModeDataPage *g_VideoModeWritePage asm("$1");
+
 int SetVideoMode(int mode) {
     int old;
 
     old = D_800956EC;
-    D_800956EC = mode;
+    g_VideoModeWritePage = (VideoModeDataPage *)0x80090000;
+    g_VideoModeWritePage->video_mode = mode;
     return old;
 }
