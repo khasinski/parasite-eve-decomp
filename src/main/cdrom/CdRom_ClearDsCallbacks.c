ec93ea64aca87433d6081dc079a87864df1a7966
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-08-13 13:11:05 +0200

    Match CD callback reset

--- src/main/cdrom/CdRom_ClearDsCallbacks.c
+++ src/main/cdrom/CdRom_ClearDsCallbacks.c
@@ -1,4 +1,6 @@
 
+/* CC1_FLAGS: -fno-schedule-insns2 */
+
 #include "pe1/psyq_cd.h"
 
 extern void CD_flush(void);
@@ -7,12 +9,22 @@ extern void VSyncCallbacks(int, int);
 extern int g_CdSyncCallback;
 extern int g_CdReadyCallback;
 
+typedef struct CdRomSystemDataPage {
+    CdRomSystemState state;
+    char reserved50[0x4A5C];
+} CdRomSystemDataPage;
+
+register CdRomSystemDataPage *g_CdRomSystemDataPage asm("$1");
+register int g_CdRomClearCallbackArg asm("$4");
+
 void CdRom_ClearDsCallbacks(void);
 
 void CdRom_ClearDsCallbacks(void) {
-    g_DsReadSysEnabled.enabled = 0;
+    g_CdRomSystemDataPage = (CdRomSystemDataPage *)0x800A0000;
+    g_CdRomSystemDataPage[-1].state.enabled = 0;
     CD_flush();
+    g_CdRomClearCallbackArg = 0;
     g_CdSyncCallback = 0;
     g_CdReadyCallback = 0;
-    VSyncCallbacks(0, 0);
+    VSyncCallbacks(g_CdRomClearCallbackArg, 0);
 }
