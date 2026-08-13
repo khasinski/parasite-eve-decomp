a9ea7f60113abb80a34cb04fae3060b296feb828
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-08-13 10:28:14 +0200

    Match SPU wait loop stack frame

--- src/main/psyq/libspu/spu_fw1ts.c
+++ src/main/psyq/libspu/spu_fw1ts.c
@@ -1,14 +1,20 @@
 
-void _spu_Fw1ts(void) {
+typedef struct SpuWaitWork {
     volatile int i;
     volatile int value;
+} SpuWaitWork;
+
+void _spu_Fw1ts(void) {
+    register SpuWaitWork *work asm("$29");
     int current;
 
-    value = 0xD;
-    i = 0;
-    while (i < 0x3C) {
-        current = value;
-        value = (((current << 1) + current) << 2) + current;
-        i = i + 1;
+    work--;
+    work->value = 0xD;
+    *(int *)&work->i = 0;
+    while (work->i < 0x3C) {
+        current = work->value;
+        work->value = (((current << 1) + current) << 2) + current;
+        work->i = work->i + 1;
     }
+    work++;
 }
