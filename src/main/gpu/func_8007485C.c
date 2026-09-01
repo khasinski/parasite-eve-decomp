a0ce5158ce347ce56a1f0e3b6df8b213c0d71c54
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-01 17:28:16 +0200

    Decompile GPU CLUT loader

---
+++ src/main/gpu/func_8007485C.c
@@ -0,0 +1,24 @@
+#include "common.h"
+#include "pe1/psyq_gpu.h"
+
+/* GCC_VERSION: 2.8.1 */
+
+int LoadImage(RECT *rect, void *pixels);
+u16 GetClut(int x, int y);
+
+u32 func_8007485C(void *pixels, int x, int y)
+{
+    RECT rect;
+    u32 clut;
+
+    rect.x = x;
+    rect.y = y;
+    rect.w = 0x100;
+    rect.h = 1;
+    LoadImage(&rect, pixels);
+    clut = GetClut(x, y);
+    clut &= 0xFFFF;
+    /* Keep the result normalization ahead of the callee-save restores. */
+    __asm__ volatile("" : "+r"(clut));
+    return clut;
+}
