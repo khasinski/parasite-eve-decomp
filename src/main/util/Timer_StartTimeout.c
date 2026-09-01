8e6f09358a40e28675545c1065fc093606396495
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-01 16:12:22 +0200

    Match Timer_StartTimeout

--- src/main/util/Timer_StartTimeout.c
+++ src/main/util/Timer_StartTimeout.c
@@ -1,24 +1,15 @@
 #include "common.h"
 
-typedef struct TimerLimitPage {
-    int limit;
-    char reserved04[0x2FD0];
-} TimerLimitPage;
+/* MASPSX_FLAGS: --store-return-delay */
 
-typedef struct TimerStartPage {
-    char reserved00[0x76D0];
-    int start;
-} TimerStartPage;
-
-register void *g_TimerWritePage asm("$1");
+extern int g_TimerTimeoutLimit;
+extern int D_800A76D0;
 
 void Timer_StartTimeout(int limit) {
     volatile u16 *timer = (volatile u16 *)0x1F801120;
     int start;
 
     start = *timer;
-    g_TimerWritePage = (void *)0x800C0000;
-    ((TimerLimitPage *)g_TimerWritePage)[-1].limit = limit;
-    g_TimerWritePage = (void *)0x800A0000;
-    ((TimerStartPage *)g_TimerWritePage)->start = start;
+    g_TimerTimeoutLimit = limit;
+    D_800A76D0 = start;
 }
