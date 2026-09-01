4bf27c5b11734f964b6244e4c1419c05bc46d26b
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-01 14:09:59 +0200

    Decompile Render_ResetScene

---
+++ src/main/main/Render_ResetScene.c
@@ -0,0 +1,84 @@
+#include "common.h"
+
+extern u8 D_800BCDC8[];
+extern u8 D_800BCE91;
+extern u8 D_800BCEA5;
+extern u16 D_800BCE9E;
+extern u16 D_800BCE8A;
+extern u16 D_800BCE9C;
+extern u16 D_800BCE88;
+extern u16 D_800BCEA2;
+extern u16 D_800BCE8E;
+extern u8 D_800BCE3C;
+extern u8 D_800BCDE0;
+extern u8 D_800BCE3A;
+extern u8 D_800BCDDE;
+extern u8 D_800BCE3B;
+extern u8 D_800BCDDF;
+extern u16 D_800BCE38;
+extern u16 D_800BCDDC;
+extern u8 D_800BCE3D;
+extern u8 D_800BCDE1;
+extern u8 D_800BCE3E;
+extern u8 D_800BCDE2;
+extern u8 D_800BCE3F;
+extern u8 D_800BCDE3;
+extern int g_ActiveDrawSlot __asm__("D_8009CDDC");
+
+void Render_InitEntityPool(int arg0);
+void SetGraphDebug(int level);
+void SetDispMask(int mask);
+void ClearImage(s16 *rect, int r, int g, int b);
+void SetDefDispEnv(void *env, int x, int y, int w, int h);
+void SetDefDrawEnv(void *env, int x, int y, int w, int h);
+void Render_StepEntityPool(void *arg0);
+
+void Render_ResetScene(int width, int height)
+{
+    s16 rect[4];
+    u8 *disp;
+    u8 *draw;
+
+    Render_InitEntityPool(0);
+    SetGraphDebug(0);
+    SetDispMask(0);
+
+    rect[2] = 0x3FF;
+    rect[3] = 0x200;
+    rect[0] = 0;
+    rect[1] = 0;
+    ClearImage(rect, 0, 0, 1);
+
+    disp = &D_800BCE91;
+    *disp = 1;
+    D_800BCEA5 = 1;
+    SetDefDispEnv(disp - 0x11, 0, height, width, height);
+    SetDefDispEnv(disp + 3, 0, 0, width, height);
+
+    draw = D_800BCDC8;
+    D_800BCE9E = 8;
+    D_800BCE8A = 8;
+    D_800BCE9C = 0;
+    D_800BCE88 = 0;
+    D_800BCE8E = D_800BCEA2 = height;
+    SetDefDrawEnv(draw, 0, 0, width, height);
+    SetDefDrawEnv(draw + 0x5C, 0, height, width, height);
+
+    D_800BCE3C = 1;
+    D_800BCDE0 = 1;
+    D_800BCE3A = 1;
+    D_800BCDDE = 1;
+    D_800BCE3B = 0;
+    D_800BCDDF = 0;
+    D_800BCE38 = 0;
+    D_800BCDDC = 0;
+    D_800BCE3D = 0;
+    D_800BCDE1 = 0;
+    D_800BCE3E = 0;
+    D_800BCDE2 = 0;
+    D_800BCE3F = 0;
+    D_800BCDE3 = 0;
+    g_ActiveDrawSlot = 0;
+
+    Render_StepEntityPool(disp - 0x11);
+}
