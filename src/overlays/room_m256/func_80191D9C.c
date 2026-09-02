5fd0e3815ad3cbebfe3c79bab25eef4fac61e256
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-02 04:18:55 +0200

    Decompile room m256 arc handler

---
+++ src/overlays/room_m256/func_80191D9C.c
@@ -0,0 +1,7 @@
+#include "../room_lib/room_lib.h"
+
+extern int RoomLib_ResetAndSignal_80192124(RoomEnt *obj);
+
+#define ROOMLIB_ADVANCE_ARC_Y_NAME func_80191D9C
+#define ROOMLIB_ADVANCE_ARC_Y_RESET RoomLib_ResetAndSignal_80192124
+#include "../room_lib/RoomLib_AdvanceArcToTargetY.inc"
