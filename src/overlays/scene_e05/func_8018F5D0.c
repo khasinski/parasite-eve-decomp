29782b9eb8346b17873c1df0a373f1f55918aba2
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-04 14:37:03 +0200

    Decompile shared special-scene render effects

---
+++ src/overlays/scene_e05/func_8018F5D0.c
@@ -0,0 +1,6 @@
+#include "common.h"
+#include "pe1/gte.h"
+#include "pe1/room_fx.h"
+#define ROOMLIB_TRANSFORM_SIXTEEN_POINT_EFFECT_FUNC func_8018F5D0
+#define Gte_ScaleMatrix func_80078CC4
+#include "../room_lib/RoomLib_TransformSixteenPointEffect.inc"
---
+++ src/overlays/scene_e05/func_8018FB44.c
@@ -0,0 +1,8 @@
+#include "common.h"
+#include "pe1/room_fx.h"
+#define ROOMLIB_DRAW_SIXTEEN_PROJECTIONS_FUNC func_8018FB44
+#define Gte_ScaleMatrix func_80078CC4
+#define ROOMLIB_SIXTEEN_PACKET_CODE D_80192194
+#define ROOMLIB_SIXTEEN_PACKET_DEPTH D_8019219A
+#define ROOMLIB_SIXTEEN_FRAME D_800942EC
+#include "../room_lib/RoomLib_DrawSixteenProjections.inc"
