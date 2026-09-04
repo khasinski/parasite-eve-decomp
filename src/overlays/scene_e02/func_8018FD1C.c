59ebfd0468cb05aaf9d34b4be085164694898c10
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-04 17:45:34 +0200

    Decompile shared scene effect callbacks

---
+++ src/overlays/scene_e02/func_8018FD1C.c
@@ -0,0 +1,9 @@
+#include "common.h"
+#include "pe1/room_fx.h"
+
+#define ROOMLIB_DRAW_SIXTEEN_PROJECTIONS_FUNC func_8018FD1C
+#define Gte_ScaleMatrix func_80078CC4
+#define ROOMLIB_SIXTEEN_PACKET_CODE D_8019572C
+#define ROOMLIB_SIXTEEN_PACKET_DEPTH D_80195732
+#define ROOMLIB_SIXTEEN_FRAME D_800942EC
+#include "../room_lib/RoomLib_DrawSixteenProjections.inc"
