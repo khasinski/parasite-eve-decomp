fb22edf7b50d05008fa875f69dc2b7fe9a715ca6
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-04 05:59:52 +0200

    Decompile shared orbit burst updater

---
+++ src/overlays/room_m186/func_80190904.c
@@ -0,0 +1,3 @@
+/* CC1_FLAGS: -fno-strength-reduce */
+#define ROOMLIB_UPDATE_ORBIT_BURST_NAME func_80190904
+#include "../room_lib/RoomLib_UpdateOrbitBurst.inc"
---
+++ src/overlays/room_m187/func_8018FA90.c
@@ -0,0 +1,3 @@
+/* CC1_FLAGS: -fno-strength-reduce */
+#define ROOMLIB_UPDATE_ORBIT_BURST_NAME func_8018FA90
+#include "../room_lib/RoomLib_UpdateOrbitBurst.inc"
