9b96d5fa427d4214d2791a8b59bd2bfb8eb9e262
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-04 06:57:59 +0200

    Decompile shared orbit burst initializer

---
+++ src/overlays/room_m186/func_801905D4.c
@@ -0,0 +1,3 @@
+/* CC1_FLAGS: -fno-strength-reduce */
+#define ROOMLIB_INIT_ORBIT_BURST_NAME func_801905D4
+#include "../room_lib/RoomLib_InitOrbitBurst.inc"
---
+++ src/overlays/room_m187/func_8018F760.c
@@ -0,0 +1,3 @@
+/* CC1_FLAGS: -fno-strength-reduce */
+#define ROOMLIB_INIT_ORBIT_BURST_NAME func_8018F760
+#include "../room_lib/RoomLib_InitOrbitBurst.inc"
