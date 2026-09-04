c0c51d4dbb93fd1a8007b9d636d7a1e550dc8735
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-04 07:41:32 +0200

    Decompile shared six-particle initializer

---
+++ src/overlays/room_m146/func_801900A8.c
@@ -0,0 +1,5 @@
+// CC1_FLAGS=-fno-strength-reduce
+#include "common.h"
+
+#define ROOMLIB_INIT_SIX_PARTICLES_FUNC func_801900A8
+#include "../room_lib/RoomLib_InitSixParticles.inc"
---
+++ src/overlays/room_m328/func_801900B4.c
@@ -0,0 +1,5 @@
+// CC1_FLAGS=-fno-strength-reduce
+#include "common.h"
+
+#define ROOMLIB_INIT_SIX_PARTICLES_FUNC func_801900B4
+#include "../room_lib/RoomLib_InitSixParticles.inc"
