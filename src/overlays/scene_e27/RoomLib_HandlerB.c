a5650c389da332e3f6b929c24d4c4eb8009a7adc
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-04 14:50:17 +0200

    Decompile shared scene motion handler B

---
+++ src/overlays/scene_e27/RoomLib_HandlerB.c
@@ -0,0 +1,6 @@
+/* MASPSX_FLAGS: --expand-div */
+#define ROOMLIB_HANDLER_B_ACTOR_PTR D_8009D254
+#define ROOMLIB_HANDLER_B_ROT_TABLE D_800966EC
+#define ROOMLIB_HANDLER_B_PHASE func_80191110
+#define ROOMLIB_HANDLER_B_RESET func_801914B8
+#include "../room_lib/RoomLib_HandlerB.inc"
