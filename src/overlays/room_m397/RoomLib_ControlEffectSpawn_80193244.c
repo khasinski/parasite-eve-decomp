924aeff241c343ae36fc989c7b15d5b34758c5ab
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-04 13:46:03 +0200

    Decompile shared effect spawn controllers

---
+++ src/overlays/room_m397/RoomLib_ControlEffectSpawn_80193244.c
@@ -0,0 +1,3 @@
+#define ROOMLIB_CONTROL_EFFECT_SPAWN_NAME func_80193244
+#define ROOMLIB_EFFECT_SPAWN_CALLBACK func_80192500
+#include "../room_lib/RoomLib_ControlEffectSpawn.inc"
---
+++ src/overlays/room_m397/RoomLib_ControlEffectSpawn_80194AAC.c
@@ -0,0 +1,3 @@
+#define ROOMLIB_CONTROL_EFFECT_SPAWN_NAME func_80194AAC
+#define ROOMLIB_EFFECT_SPAWN_CALLBACK func_80193D7C
+#include "../room_lib/RoomLib_ControlEffectSpawn.inc"
