2984c19d95061863cea958ed35a12e5941b113fd
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-04 06:49:01 +0200

    Decompile shared orbit render initializer

---
+++ src/overlays/room_m186/func_80190048.c
@@ -0,0 +1,2 @@
+#define ROOMLIB_INIT_ORBIT_RENDER_NAME func_80190048
+#include "../room_lib/RoomLib_InitOrbitRenderState.inc"
---
+++ src/overlays/room_m187/func_8018F1D4.c
@@ -0,0 +1,3 @@
+#define ROOMLIB_INIT_ORBIT_RENDER_NAME func_8018F1D4
+#define ROOMLIB_INIT_ORBIT_RENDER_COMPACT_GLOBALS
+#include "../room_lib/RoomLib_InitOrbitRenderState.inc"
