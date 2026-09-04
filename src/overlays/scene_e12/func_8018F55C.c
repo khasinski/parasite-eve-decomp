93c0a4a43977a35a2307d00b3227ab2ab3e7b955
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-04 19:46:59 +0200

    Decompile paired scene emitter updates

---
+++ src/overlays/scene_e12/func_8018F55C.c
@@ -0,0 +1,3 @@
+#define ROOMLIB_UPDATE_PAIRED_EMITTER_NAME func_8018F55C
+#define ROOMLIB_UPDATE_PAIRED_EMITTER_TABLE D_80191AD8
+#include "../room_lib/RoomLib_UpdatePairedEmitter.inc"
---
+++ src/overlays/scene_e14/func_8018F518.c
@@ -0,0 +1,3 @@
+#define ROOMLIB_UPDATE_PAIRED_EMITTER_NAME func_8018F518
+#define ROOMLIB_UPDATE_PAIRED_EMITTER_TABLE D_80190DF0
+#include "../room_lib/RoomLib_UpdatePairedEmitter.inc"
