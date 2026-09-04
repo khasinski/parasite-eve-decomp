c900d8020dd4ebc2fa43462d84de57841a36fc6a
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-04 06:53:37 +0200

    Decompile shared orbit sprite initializer

---
+++ src/overlays/room_m187/func_8018FBE4.c
@@ -0,0 +1,3 @@
+#define ROOMLIB_INIT_ORBIT_SPRITE_NAME func_8018FBE4
+#define ROOMLIB_INIT_ORBIT_SPRITE_COMPACT_SEEDS
+#include "../room_lib/RoomLib_InitOrbitSprite.inc"
---
+++ src/overlays/room_m388/func_80190A58.c
@@ -0,0 +1,2 @@
+#define ROOMLIB_INIT_ORBIT_SPRITE_NAME func_80190A58
+#include "../room_lib/RoomLib_InitOrbitSprite.inc"
