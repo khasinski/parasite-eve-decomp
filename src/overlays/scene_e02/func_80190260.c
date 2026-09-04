6290e2c493f0006ae499318dc2a84a1f9f62d01f
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-04 20:31:08 +0200

    Decompile scene six-particle trail effects

---
+++ src/overlays/scene_e02/func_80190260.c
@@ -0,0 +1,4 @@
+#include "common.h"
+
+#define ROOMLIB_INIT_SIX_PARTICLES_FUNC func_80190260
+#include "../room_lib/RoomLib_InitSixParticles.inc"
---
+++ src/overlays/scene_e02/func_8019061C.c
@@ -0,0 +1,2 @@
+#define ROOMLIB_UPDATE_TRAIL_PARTICLE_NAME func_8019061C
+#include "../room_lib/RoomLib_UpdateTrailParticle.inc"
