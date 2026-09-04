919b494877570a523a97514d51fdb04fc9ce0393
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-04 05:55:56 +0200

    Decompile shared orbit batch renderer

---
+++ src/overlays/room_m187/func_8018F510.c
@@ -0,0 +1,4 @@
+/* CC1_FLAGS: -fno-strength-reduce */
+#define ROOMLIB_DRAW_ORBIT_BATCH_NAME func_8018F510
+#define ROOMLIB_DRAW_ORBIT_BATCH_DEPTH D_8019009A
+#include "../room_lib/RoomLib_DrawOrbitBatch.inc"
---
+++ src/overlays/room_m385/func_80190384.c
@@ -0,0 +1,4 @@
+/* CC1_FLAGS: -fno-strength-reduce */
+#define ROOMLIB_DRAW_ORBIT_BATCH_NAME func_80190384
+#define ROOMLIB_DRAW_ORBIT_BATCH_DEPTH D_8019437A
+#include "../room_lib/RoomLib_DrawOrbitBatch.inc"
