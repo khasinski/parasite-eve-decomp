a6b813b4107e73beede2273bc073ba558a0ddbb4
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-04 08:52:30 +0200

    Decompile shared twelve-element effect initializer

---
+++ src/overlays/room_m186/func_8018F598.c
@@ -0,0 +1,6 @@
+#include "common.h"
+#include "pe1/gte.h"
+#include "pe1/room_fx.h"
+
+#define ROOMLIB_INIT_TWELVE_EFFECT_FUNC func_8018F598
+#include "../room_lib/RoomLib_InitTwelveElementEffect.inc"
