e979ba39d3227d7d2e1aed2297e49d5823d22260
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-08 15:52:56 +0200

    Consolidate GPU clear image translation unit

--- src/main/gpu/clear_image.c
+++ src/main/gpu/clear_image.c
@@ -6,9 +6,20 @@
 #include "pe1/psyq_gpu.h"
 #include "pe1/gpu_state.h"
 
+extern char D_800118BC[];
 extern char D_800118C8[];
 
 void checkRECT(char *message, RECT *rect);
+
+int ClearImage(RECT *rect, u8 r, u8 g, u8 b) {
+    GpuCallbacks *callbacks;
+    u32 color;
+
+    checkRECT(D_800118BC, rect);
+    color = ((u32)b << 16) | ((u32)g << 8) | r;
+    callbacks = D_80095744;
+    return callbacks->addque2(callbacks->clr, rect, 8, color);
+}
 
 int ClearImage2(RECT *rect, u8 r, u8 g, u8 b) {
     GpuCallbacks *callbacks;
