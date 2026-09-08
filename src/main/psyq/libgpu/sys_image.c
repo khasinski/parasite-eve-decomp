d61f8a1f8df32957cb5dfb96f062c2cd798e2bdd
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-08 19:19:03 +0200

    Merge Psy-Q GPU image translation unit

--- src/main/psyq/libgpu/sys_image.c
+++ src/main/psyq/libgpu/sys_image.c
@@ -67,3 +67,70 @@ int ClearImage2(RECT *rect, u8 r, u8 g, u8 b) {
     callbacks = D_80095744;
     return callbacks->addque2(callbacks->clr, rect, 8, color);
 }
+
+/* GCC_VERSION: 2.8.1 */
+/* CC1_FLAGS: -mno-split-addresses */
+
+#include "common.h"
+#include "pe1/gpu_callbacks.h"
+#include "pe1/psyq_gpu.h"
+
+extern char D_800118D4[];
+extern char D_800118E0[];
+extern char D_800118EC[];
+extern GpuCallbacks *D_80095744;
+extern u32 D_800957EC[];
+
+void checkRECT(char *message, RECT *rect);
+
+int LoadImage(RECT *rect, void *pixels) {
+    GpuCallbacks *callbacks;
+
+    checkRECT(D_800118D4, rect);
+    callbacks = D_80095744;
+    return callbacks->addque2(callbacks->u20.load, rect, 8, pixels);
+}
+
+int StoreImage(RECT *rect, void *pixels) {
+    GpuCallbacks *callbacks;
+
+    checkRECT(D_800118E0, rect);
+    callbacks = D_80095744;
+    return callbacks->addque2(callbacks->u1c.store, rect, 8, pixels);
+}
+
+int MoveImage(RECT *rect, int x, int y) {
+    checkRECT(D_800118EC, rect);
+    if (rect->w == 0 || rect->h == 0) {
+        return -1;
+    }
+    {
+        register u32 *packetData asm("$3");
+        GpuCallbacks *callbacks;
+        register u32 sourcePosition asm("$5");
+        u32 *packet;
+        int packetSize;
+        u32 destinationPosition;
+        register u32 destinationX asm("$4");
+        u32 size;
+
+        destinationPosition = (u32)y << 16;
+        packetData = D_800957EC;
+        asm("" : "+r"(packetData));
+        destinationX = x & 0xFFFF;
+        destinationPosition |= destinationX;
+        sourcePosition = *(u32 *)rect;
+        callbacks = D_80095744;
+        asm("" : "+r"(sourcePosition), "+r"(callbacks));
+        packetSize = 20;
+        asm("" : "+r"(packetSize));
+        packetData[1] = destinationPosition;
+        *packetData = sourcePosition;
+        size = *((u32 *)rect + 1);
+        packet = packetData - 2;
+        packetData[2] = size;
+        asm("" : : "r"(size) : "memory");
+        return callbacks->addque2(callbacks->u18.moveImage, packet, packetSize,
+                                  0);
+    }
+}
