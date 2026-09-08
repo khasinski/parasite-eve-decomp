7948e4ca2c6a4fa7c165730efd4573fa992124f5
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 00:04:29 +0200

    Merge CD-ROM callback setter translation unit

--- src/main/cdrom/CdRom_SetPollCallback.c
+++ src/main/cdrom/CdRom_SetPollCallback.c
@@ -4,3 +4,18 @@ void CdRom_SetPollCallback(unsigned int value) {
     g_DsCallbackSetPage = (DsCallbackSetPage *)0x800A0000;
     g_DsCallbackSetPage->poll = value;
 }
+
+void CdRom_SetSyncCallback(unsigned int value) {
+    g_DsCallbackSetPage = (DsCallbackSetPage *)0x800A0000;
+    g_DsCallbackSetPage->sync = value;
+}
+
+void CdRom_SetReadyCallback(unsigned int value) {
+    g_DsCallbackSetPage = (DsCallbackSetPage *)0x800A0000;
+    g_DsCallbackSetPage->ready = value;
+}
+
+void CdRom_SetDispatchCallback(unsigned int value) {
+    g_DsCallbackSetPage = (DsCallbackSetPage *)0x800A0000;
+    g_DsCallbackSetPage->dispatch = value;
+}
--- src/main/cdrom/CdRom_SetSyncCallback.c
+++
@@ -1,6 +0,0 @@
-#include "pe1/cdrom_callback_internal.h"
-
-void CdRom_SetSyncCallback(unsigned int value) {
-    g_DsCallbackSetPage = (DsCallbackSetPage *)0x800A0000;
-    g_DsCallbackSetPage->sync = value;
-}
