d898b9c53913e8deeaec0e65e89f66111c0d6cef
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-08 13:02:24 +0200

    Reconstruct Psy-Q GPU transfer translation unit

--- src/main/psyq/libgpu/sys_transfer.c
+++ src/main/psyq/libgpu/sys_transfer.c
@@ -5,11 +5,29 @@
 #include "pe1/gpu_callbacks.h"
 #include "pe1/psyq_gpu.h"
 
+extern char D_800118D4[];
+extern char D_800118E0[];
 extern char D_800118EC[];
 extern GpuCallbacks *D_80095744;
 extern u32 D_800957EC[];
 
 void checkRECT(char *message, RECT *rect);
+
+int LoadImage(RECT *rect, void *pixels) {
+    GpuCallbacks *callbacks;
+
+    checkRECT(D_800118D4, rect);
+    callbacks = D_80095744;
+    return callbacks->addque2(callbacks->u20.load, rect, 8, pixels);
+}
+
+int StoreImage(RECT *rect, void *pixels) {
+    GpuCallbacks *callbacks;
+
+    checkRECT(D_800118E0, rect);
+    callbacks = D_80095744;
+    return callbacks->addque2(callbacks->u1c.store, rect, 8, pixels);
+}
 
 int MoveImage(RECT *rect, int x, int y) {
     checkRECT(D_800118EC, rect);
