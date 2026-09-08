1032764af57105b8eecec2b50eecca2f1045611a
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-08 18:39:44 +0200

    Merge AKAO voice envelope translation unit

--- src/main/akao/voice_envelopes.c
+++ src/main/akao/voice_envelopes.c
@@ -1,3 +1,17 @@
+void Akao_RemoveVoice(int *arg0, int arg1) {
+    int i = 0;
+    int value = 0x18;
+
+    arg0 += 0x3C;
+    do {
+        if (arg1 == *arg0) {
+            *arg0 = value;
+        }
+        i++;
+        arg0 += 0x47;
+    } while ((unsigned int)i < 0x18);
+}
+
 #include "common.h"
 void SpuGetVoiceEnvelope();
 void Akao_RemoveVoice();
