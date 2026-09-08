f6afbc3722973549df5a757695daec19c7ba6bee
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-08 15:45:00 +0200

    Reconstruct Psy-Q GPU display translation unit

--- src/main/psyq/libgpu/sys_display.c
+++ src/main/psyq/libgpu/sys_display.c
@@ -37,3 +37,23 @@ void SetDispMask(int mask) {
     }
     callbacks->callback10(command);
 }
+
+#include "include_asm.h"
+#include "pe1/gpu_state.h"
+
+extern char D_80011884[];
+
+int DrawSync(int arg0) {
+    register int saved asm("$16");
+    void (*fn)(char *, int);
+    GpuCallbacks *callbacks;
+
+    saved = arg0;
+    if (D_8009574C.queueState.debugLevel >= 2) {
+        fn = D_80095748;
+        fn(D_80011884, saved);
+    }
+
+    callbacks = D_80095744;
+    callbacks->callback3C(saved);
+}
