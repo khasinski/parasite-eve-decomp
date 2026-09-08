85c67e98a0832c58eba89ebcf4c1ae2c4e8e8473
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 00:19:38 +0200

    Merge CD-ROM sync adapter translation unit

--- src/main/cdrom/CdRom_Sync.c
+++ src/main/cdrom/CdRom_Sync.c
@@ -3,3 +3,9 @@ extern void CD_sync(int, int);
 void CdRom_Sync(int result) {
     CD_sync(1, result);
 }
+
+extern void CD_ready(int, int);
+
+void CdRom_SendReadyCommand(int result) {
+    CD_ready(1, result);
+}
