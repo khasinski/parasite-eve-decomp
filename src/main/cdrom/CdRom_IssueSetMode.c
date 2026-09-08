1636b92e5db61e666b63d938880ce08db0cffd23
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-08 17:35:59 +0200

    Share Psy-Q CD timeout declaration

--- src/main/cdrom/CdRom_IssueSetMode.c
+++ src/main/cdrom/CdRom_IssueSetMode.c
@@ -1,10 +1,10 @@
-extern int D_8009B598;
+#include "pe1/psyq_cd.h"
 
 int CdRom_SendCmd(int arg0);
 
 int func_8007FCBC(int arg0) {
     volatile int *ptr;
-    ptr = &D_8009B598;
+    ptr = &g_CdRomCmdTimeout;
     if (*ptr > 0) {
         return 0;
     }
