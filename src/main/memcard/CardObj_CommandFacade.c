4e0f17ac6096b41edf546ea1970a0130ebee7cde
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 05:26:23 +0200

    Merge memory card command facade translation unit

--- src/main/memcard/CardObj_CommandFacade.c
+++ src/main/memcard/CardObj_CommandFacade.c
@@ -23,3 +23,18 @@ int func_8008284C(int channel, int index0, int index1) {
     }
     return *(unsigned char *)(*(int *)(entry + 4) + index1);
 }
+void func_80083BB8(CardObj *obj, int command);
+
+void CardObj_StartCommand4D(int channel, int command) {
+    func_80083BB8(D_8009B738(), command);
+}
+void CardObj_BeginReadIdCommand(CardObj *obj, int byte1, int byte2);
+
+void CardObj_StartCommandWithBytes(int channel, unsigned char byte1, unsigned char byte2) {
+    CardObj_BeginReadIdCommand(D_8009B738(), byte1, byte2);
+}
+void func_800835A4(CardObj *obj, int payload, int size);
+
+void CardObj_SetCommandPayload(int channel, int payload, int size) {
+    func_800835A4(D_8009B738(), payload, size);
+}
