434892e3b01cc5d2e363319f4a2f4290de0a4bf6
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 05:14:11 +0200

    Merge menu grid-cell renderer translation unit

--- src/main/menu/Menu_DrawGridCellVariants.c
+++ src/main/menu/Menu_DrawGridCellVariants.c
@@ -12,7 +12,22 @@ void func_80050F64(int index) {
     if (index != MenuWidget_GridCellIndex(D_8009CEF4)) {
         Draw_SetTextDimmed(1);
     }
-
     Draw_OffsetCursor(-2, -2);
     Draw_AllocSprite(index + 0x63);
+}
+
+void func_80050FB8(int index) {
+    if (index != MenuWidget_GridCellIndex(D_8009CEF4)) {
+        Draw_SetTextDimmed(1);
+    }
+    Draw_OffsetCursor(-2, -2);
+    Draw_AllocSprite(index + 0x66);
+}
+
+void func_8005100C(int index) {
+    if (index != MenuWidget_GridCellIndex(D_8009CEF4)) {
+        Draw_SetTextDimmed(1);
+    }
+    Draw_OffsetCursor(-2, -2);
+    Draw_AllocSprite(index + 0x62);
 }
