6efab4db380b5dcee6acba0754fe4a23b0d2ad57
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 05:22:18 +0200

    Merge menu widget grid node translation unit

--- src/main/menu/MenuWidget_GridNode.c
+++ src/main/menu/MenuWidget_GridNode.c
@@ -1,8 +1,42 @@
-#include "common.h"
-#include "pe1/menu_widget.h"
 /* CC1_FLAGS: -G8 */
 /* MASPSX_FLAGS: -G8 */
 
+#include "pe1/menu_widget.h"
+
+void MenuWidget_ClearCursorY(MenuWidgetNode *ptr) {
+    if (ptr) {
+        ptr->cursor_y = 0;
+    }
+}
+
+void MenuWidget_SetCursorY(MenuWidgetNode *ptr) {
+    if (ptr) {
+        ptr->cursor_y = 1;
+    }
+}
+
+int MenuWidget_IsCursorYClear(MenuWidgetNode *ptr) {
+    if (ptr) {
+        return ptr->cursor_y == 0;
+    }
+
+    return 0;
+}
+
+MenuWidgetNode *MenuWidget_FindLastMode1WithCursorX(void) {
+    MenuWidgetNode *node;
+    MenuWidgetNode *result;
+
+    node = g_MenuWidgetActiveListHead;
+    result = 0;
+    while (node != 0) {
+        if (node->mode == 1 && node->cursor_x != 0) {
+            result = node;
+        }
+        node = node->next;
+    }
+    return result;
+}
 void BoundsCheck_AssertStub(int arg0);
 void Menu_StepScrollCursor(void);
 void Draw_SwapPrimBuffers(void *node);
