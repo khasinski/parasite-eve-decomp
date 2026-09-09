6324a658a7313f5b7ca5608ec41cc67e67d32a72
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 10:02:13 +0200

    Merge entity movement script control translation unit

--- src/main/entity/Entity_MovementScriptControl.c
+++ src/main/entity/Entity_MovementScriptControl.c
@@ -4,6 +4,27 @@
 #include "pe1/field_actor.h"
 
 extern FieldActor *g_CurrentEntity[];
+extern int g_SceneDataTable0;
+extern int *g_TaskNodePool;
+
+int Entity_YieldWhileMoving(void) {
+    int ret;
+
+    ret = 0;
+    if ((g_CurrentEntity[0]->flags & 2) == 0) {
+        return 1;
+    }
+
+    g_SceneDataTable0 -= 8;
+    g_TaskNodePool[4] = 1;
+    return ret;
+}
+/* CC1_FLAGS: -G8 */
+/* MASPSX_FLAGS: -G8 */
+
+#include "pe1/field_actor.h"
+
+extern FieldActor *g_CurrentEntity[];
 extern int *g_TaskNodePool;
 
 int Entity_SelectFieldSetter(int **arg0) {
