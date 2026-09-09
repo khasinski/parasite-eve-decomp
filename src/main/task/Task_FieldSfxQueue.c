a937324adb23c7b85610fe21933ad146fbbd0051
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 07:15:26 +0200

    Merge field SFX queue translation unit

--- src/main/task/Task_FieldSfxQueue.c
+++ src/main/task/Task_FieldSfxQueue.c
@@ -5,6 +5,18 @@
 
 FieldSfxU8 g_FieldSfxQueueCount;
 
+void Task_ClearSfxTable(void) {
+    unsigned int i;
+    unsigned int j;
+
+    for (i = 0; i < 28; i++) {
+        for (j = 0; j < 3; j++) {
+            ((int *)&g_FieldSfxQueue[i])[j] = 0;
+        }
+    }
+    g_FieldSfxQueueCount = 0;
+}
+
 void Task_QueueFieldSfx(int arg0, int arg1, int arg2, int arg3, int arg4) {
     FieldSfxQueueEntry *entry = &g_FieldSfxQueue[g_FieldSfxQueueCount];
 
