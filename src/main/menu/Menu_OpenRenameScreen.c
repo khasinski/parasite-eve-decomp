0e2fa117e84e6c735d614a0befdb73e24133c9ed
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 06:24:22 +0200

    Merge rename screen translation unit

--- src/main/menu/Menu_OpenRenameScreen.c
+++ src/main/menu/Menu_OpenRenameScreen.c
@@ -17,8 +17,7 @@ extern M2C_UNK D_800923A0[];
 #define D_800923A0 (D_800923A0[0])
 extern s32 g_GameState[];
 #define g_GameState (g_GameState[0])
-extern M2C_UNK Menu_DrawSaveMetadataPreview[];
-#define Menu_DrawSaveMetadataPreview (Menu_DrawSaveMetadataPreview[0])
+void Menu_DrawSaveMetadataPreview(void);
 extern M2C_UNK Menu_StepNameEntry[];
 #define Menu_StepNameEntry (Menu_StepNameEntry[0])
 extern M2C_UNK Menu_StepNameEntryAlt[];
@@ -83,4 +82,55 @@ void Menu_OpenRenameScreen(s32 arg0) {
     Save_LoadMetadataWindowText();
     Inv_ClearEquipFlagForKind(g_MenuRenameTargetRecord);
     g_GameState = g_GameState | 0x8000;
+}
+
+#include "common.h"
+#include "pe1/save.h"
+/* CC1_FLAGS: -G8 */
+/* MASPSX_FLAGS: -G8 */
+
+#define NULL ((void *)0)
+#include "../../../tools/m2c/m2c_macros.h"
+M2C_UNK Sfx_CursorRenderData();
+M2C_UNK Draw_OffsetCursor();
+M2C_UNK Draw_AllocSprite();
+s32 Draw_MeasureTextWidth();
+M2C_UNK Draw_PrintRawText();
+M2C_UNK Draw_EmitWipeBarRect();
+
+void Menu_DrawSaveMetadataPreview(void);
+
+void Menu_DrawSaveMetadataPreview(void) {
+    u8 *var_a0;
+    s32 temp_v0;
+    register s32 var_a0_2 asm("$4");
+    s32 var_s0;
+    volatile s32 frame_pad[2];
+
+    (void) frame_pad;
+    if (g_MenuRenameTargetRecord != NULL) {
+        Draw_OffsetCursor(0x1C, 0xC);
+        Sfx_CursorRenderData(g_MenuRenameTargetRecord);
+        var_a0 = (u8 *)g_CursorRenderMetadataWindows;
+        if (g_MenuRenameTargetRecord->kind == ITEM_KIND_ARMOR) {
+            var_a0 = g_CursorRenderMetadataWindows[1].text;
+        }
+        var_a0_2 = Draw_MeasureTextWidth(var_a0) + 0x14;
+    } else {
+        Draw_OffsetCursor(2, 2);
+        Draw_AllocSprite(0x47);
+        Draw_OffsetCursor(0x3C, 8);
+        Draw_PrintRawText(Save_GetActiveMetadataBuffer());
+        var_a0_2 = Draw_MeasureTextWidth(Save_GetActiveMetadataBuffer());
+    }
+    Draw_OffsetCursor(var_a0_2, 0xC);
+    temp_v0 = Save_GetMetadataRemainingChars();
+    var_s0 = temp_v0 - 1;
+    if (temp_v0 != 0) {
+        do {
+            Draw_EmitWipeBarRect(9, 4, 0);
+            Draw_OffsetCursor(0xB, 0);
+            var_s0 -= 1;
+        } while (var_s0 != -1);
+    }
 }
