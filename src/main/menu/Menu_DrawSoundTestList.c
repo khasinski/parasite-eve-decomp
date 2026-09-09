c19cbec7888589de9cde146130e4be0815c809fc
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 06:43:13 +0200

    Merge menu option list adapter translation unit

--- src/main/menu/Menu_DrawSoundTestList.c
+++
@@ -1,6 +0,0 @@
-void Menu_SoundTestSelect(void);
-void MenuWidget_DrawList(void *arg0, void (*arg1)(void));
-
-void Menu_DrawSoundTestList(void *arg0) {
-    MenuWidget_DrawList(arg0, Menu_SoundTestSelect);
-}
