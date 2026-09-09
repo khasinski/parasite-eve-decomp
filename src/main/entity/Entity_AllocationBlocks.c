6e9ab1445df0eaffcfe796e7026960d0bbcc89e2
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 10:17:43 +0200

    Merge entity allocation blocks translation unit

--- src/main/entity/Entity_AllocationBlocks.c
+++ src/main/entity/Entity_AllocationBlocks.c
@@ -79,3 +79,20 @@ block_20:
     }
     goto loop_8;
 }
+
+extern char g_EntityAllocBlockTable[];
+
+void Entity_FreeAllocationBlock(int arg0) {
+    int i;
+    int offset;
+
+    i = 0;
+    while ((unsigned int)(i & 0xFF) < 0x10U) {
+        offset = (i & 0xFF) << 3;
+        if (*(int *)(g_EntityAllocBlockTable + offset) == arg0) {
+            *(int *)(g_EntityAllocBlockTable + offset) = 0;
+            return;
+        }
+        i++;
+    }
+}
