944aebff2bc2d54233724ce2907d6d13da7a9f73
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 04:01:14 +0200

    Merge rectangle primitive allocators

---
+++ src/main/main/Draw_AllocRect.c
@@ -0,0 +1,92 @@
+#include "common.h"
+/* CC1_FLAGS: -G8 */
+/* MASPSX_FLAGS: -G8 */
+
+#define NULL ((void *)0)
+#include "m2c_macros.h"
+
+#include "pe1/psyq_gpu.h"
+
+M2C_UNK BoundsCheck_AssertStub();
+void SetDrawArea(GpuCmdPacket *, RECT *);
+extern u32 g_ActiveDrawBuffer;
+extern s32 g_DrawPacketBufferBase;
+extern s32 g_DrawBufferIndex;
+extern s32 *g_OtListTail;
+
+void Draw_AllocPrimRectFull(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
+    RECT rect;
+    u32 oldPacket;
+    u32 nextPacket;
+    GpuCmdPacket *packet;
+    RECT *rectPtr;
+
+    if (g_DrawBufferIndex != 0) {
+        arg1 += 0xE0;
+    }
+    packet = NULL;
+    rect.x = arg0;
+    oldPacket = g_ActiveDrawBuffer;
+    rectPtr = &rect;
+    rect.y = arg1;
+    rect.w = arg2;
+    nextPacket = oldPacket + 0xC;
+    rect.h = arg3;
+    if (nextPacket < (u32) (g_DrawPacketBufferBase + 0x4000)) {
+        g_ActiveDrawBuffer = nextPacket;
+        packet = (GpuCmdPacket *) oldPacket;
+    } else {
+        BoundsCheck_AssertStub(1);
+    }
+    if (packet != NULL) {
+        SetDrawArea(packet, rectPtr);
+    }
+    packet->u0.tag = (packet->u0.tag & 0xFF000000) | (*g_OtListTail & 0xFFFFFF);
+    *g_OtListTail = (*g_OtListTail & 0xFF000000) | ((s32) packet & 0xFFFFFF);
+}
+#include "common.h"
+/* CC1_FLAGS: -G8 */
+/* MASPSX_FLAGS: -G8 */
+
+#define NULL ((void *)0)
+#include "m2c_macros.h"
+
+#include "pe1/psyq_gpu.h"
+
+M2C_UNK BoundsCheck_AssertStub();
+void SetDrawArea(GpuCmdPacket *, RECT *);
+extern u32 g_ActiveDrawBuffer;
+extern s32 g_DrawPacketBufferBase;
+extern s32 g_DrawBufferIndex;
+extern s32 *g_OtListTail;
+
+void Draw_AllocPrimRect(void) {
+    RECT rect;
+    u32 oldPacket;
+    u32 nextPacket;
+    GpuCmdPacket *packet;
+    RECT *rectPtr;
+
+    rect.w = 0x140;
+    rect.x = 0;
+    rect.y = 0;
+    rect.h = 0xE0;
+    if (g_DrawBufferIndex != 0) {
+        rect.y = 0xE0;
+    }
+    packet = NULL;
+    oldPacket = g_ActiveDrawBuffer;
+    nextPacket = oldPacket + 0xC;
+    rectPtr = &rect;
+    if (nextPacket < (u32) (g_DrawPacketBufferBase + 0x4000)) {
+        g_ActiveDrawBuffer = nextPacket;
+        packet = (GpuCmdPacket *) oldPacket;
+    } else {
+        BoundsCheck_AssertStub(1);
+    }
+    if (packet != NULL) {
+        SetDrawArea(packet, rectPtr);
+    }
+    packet->u0.tag = (packet->u0.tag & 0xFF000000) | (*g_OtListTail & 0xFFFFFF);
+    *g_OtListTail = (*g_OtListTail & 0xFF000000) | ((s32) packet & 0xFFFFFF);
+}
