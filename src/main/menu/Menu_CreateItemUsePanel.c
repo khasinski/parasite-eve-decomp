4e50dd2c142b9184a399bb4b2778b15a678b5c4e
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 06:28:24 +0200

    Merge item use panel translation unit

--- src/main/menu/Menu_CreateItemUsePanel.c
+++ src/main/menu/Menu_CreateItemUsePanel.c
@@ -14,8 +14,7 @@ void *MenuWidget_CreateNode();
 M2C_UNK MenuWidget_ClearColumnLayout();
 extern s32 g_InvItemUsableFlag;
 extern void *g_MenuSelectedItemData;
-extern M2C_UNK Menu_DrawEquipStatsPanel[];
-#define Menu_DrawEquipStatsPanel (Menu_DrawEquipStatsPanel[0])
+void Menu_DrawEquipStatsPanel(void);
 extern M2C_UNK Menu_HandleItemInput[];
 #define Menu_HandleItemInput (Menu_HandleItemInput[0])
 extern M2C_UNK Menu_DrawEquipItemName[];
@@ -66,4 +65,42 @@ void Menu_CreateItemUsePanel(void) {
     }
     M2C_FIELD(MenuWidget_CreateSimpleNode(0x13, 0, 0, 0), M2C_UNK **, 0x30) = &Menu_DrawEquipItemName;
     Queue_Init();
+}
+
+#include "common.h"
+/* CC1_FLAGS: -G8 */
+/* MASPSX_FLAGS: -G8 */
+
+#define NULL ((void *)0)
+#include "../../../tools/m2c/m2c_macros.h"
+M2C_UNK Menu_DrawEquipStatsDelta(void *);                      /* extern */
+M2C_UNK Draw_OffsetCursor(M2C_UNK, M2C_UNK);            /* extern */
+M2C_UNK Draw_AllocSprite(M2C_UNK);                     /* extern */
+M2C_UNK Draw_PrintNumberWidth4Unk(u8);                          /* extern */
+M2C_UNK Draw_PrintSignedNumberWidth4(s16);                         /* extern */
+extern void *g_MenuSelectedItemData;
+
+void Menu_DrawEquipStatsPanel(void) {
+    void *temp_s0;
+
+    Menu_DrawEquipStatsDelta(g_MenuSelectedItemData);
+    temp_s0 = g_MenuSelectedItemData;
+    if (temp_s0 != NULL) {
+        Draw_OffsetCursor(0x2A, -0xC);
+        Draw_PrintNumberWidth4Unk(M2C_FIELD(temp_s0, u8 *, 9));
+        Draw_OffsetCursor(5, 0);
+        Draw_PrintSignedNumberWidth4(M2C_FIELD(temp_s0, s16 *, 0x12));
+        Draw_OffsetCursor(-0x2D, -0xE);
+        Draw_PrintNumberWidth4Unk(M2C_FIELD(temp_s0, u8 *, 8));
+        Draw_OffsetCursor(5, 0);
+        Draw_PrintSignedNumberWidth4(M2C_FIELD(temp_s0, s16 *, 0x10));
+        Draw_OffsetCursor(-0x2D, -0xE);
+        Draw_PrintNumberWidth4Unk(M2C_FIELD(temp_s0, u8 *, 7));
+        Draw_OffsetCursor(5, 0);
+        Draw_PrintSignedNumberWidth4(M2C_FIELD(temp_s0, s16 *, 0xE));
+        Draw_OffsetCursor(-0x2D, -0xA);
+        Draw_AllocSprite(0x87);
+        Draw_OffsetCursor(0x19, 0);
+        Draw_AllocSprite(0x88);
+    }
 }
