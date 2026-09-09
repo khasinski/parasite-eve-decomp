0ac91222a92bef389820771282ae19b04894399a
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 07:01:35 +0200

    Merge entity parent-link translation unit

--- src/main/task/Task_EntityParentLinks.c
+++ src/main/task/Task_EntityParentLinks.c
@@ -53,3 +53,55 @@ link:
     }
     return 1;
 }
+
+int Task_ClearEntityParentLink(void) {
+    FieldActor *current;
+    FieldActor *it;
+
+    current = g_CurrentEntity;
+    it = g_FieldActorListHead;
+    current->parent = 0;
+    current->flags &= 0xFF9FFFFF;
+
+    if (it != 0) {
+        do {
+            if (it != current) {
+                if (it->parent == current->parent) {
+                    return 1;
+                }
+            }
+            it = it->next;
+        } while (it != 0);
+    }
+
+    {
+        FieldActor *tail_current;
+        int clear_mask;
+        tail_current = g_CurrentEntity;
+        clear_mask = 0xFFEFFFFF;
+        tail_current->parent->flags &= clear_mask;
+    }
+    return 1;
+}
+
+int Entity_ClearParentLink(void) {
+    int clear_current = 0xFFEFFFFF;
+    FieldActor *current = g_CurrentEntity;
+    FieldActor *it = g_FieldActorListHead;
+
+    current->flags &= clear_current;
+
+    if (it != 0) {
+        int clear_child = 0xFF9FFFFF;
+
+        do {
+        if (it->parent == current) {
+            it->parent = 0;
+                it->flags &= clear_child;
+        }
+            it = it->next;
+        } while (it != 0);
+    }
+
+    return 1;
+}
