3f46464b02114aeee8945401e04d684d355939d1
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 07:36:13 +0200

    Merge field engine color table transfer translation unit

--- src/main/engine/FieldEng_ColorTableTransfer.c
+++ src/main/engine/FieldEng_ColorTableTransfer.c
@@ -11,3 +11,15 @@ void func_800C6EF8(char *data) {
         *dst++ = *src++;
     }
 }
+
+
+void func_800C6F4C(char *data) {
+    int i = 0;
+    u32 *src = D_800E2370;
+    u32 *dst = (u32 *)(data + *(u16 *)(data + 0x8));
+
+    while (i < *(u16 *)(data + 0xA)) {
+        i++;
+        *dst++ = *src++;
+    }
+}
