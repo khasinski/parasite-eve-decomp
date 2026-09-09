95faf99ea394c562899613d45c271cfe6912d320
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 10:09:37 +0200

    Merge GTE lighting state translation unit

--- src/main/gpu/Gte_LightingState.c
+++ src/main/gpu/Gte_LightingState.c
@@ -1,4 +1,27 @@
 #include "common.h"
+void Gte_SetBackColor(u16 *dst, int r, int g, int b) {
+    register int sr asm("t4");
+    register int sg asm("t5");
+    register int sb asm("t6");
+
+    sr = r << 4;
+    sg = g << 4;
+    sb = b << 4;
+    asm volatile("ctc2 %0,$13" : : "r"(sr));
+    asm volatile("ctc2 %0,$14" : : "r"(sg));
+    asm volatile("ctc2 %0,$15" : : "r"(sb));
+
+    dst[0] = 0;
+    dst[1] = 0;
+    dst[2] = 0;
+    dst[3] = 0;
+    dst[4] = 0;
+    dst[5] = 0;
+    dst[6] = 0;
+    dst[7] = 0;
+    dst[8] = 0;
+}
+#include "common.h"
 void Gte_SetLightColor(u8 *state, int index, int r, int g, u16 b) {
     u8 *byte_slot;
     register u32 m0 asm("t4");
