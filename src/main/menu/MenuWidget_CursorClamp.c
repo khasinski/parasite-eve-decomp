eb63edea6f41b548f7cb20fefeb016b9d642785b
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 05:24:23 +0200

    Merge menu widget cursor clamp translation unit

--- src/main/menu/MenuWidget_CursorClamp.c
+++ src/main/menu/MenuWidget_CursorClamp.c
@@ -1,5 +1,25 @@
 #include "pe1/menu_widget.h"
 
+void MenuWidget_ClampScroll(MenuWidgetNode *ptr) {
+    int y;
+    int top;
+    int height;
+    int next;
+
+    y = ptr->cursor_y;
+    top = ptr->scroll_y;
+    if (y < top) {
+        ptr->scroll_y = y;
+    } else {
+        height = ptr->visible_rows;
+        if (y >= top + height) {
+            next = y - height;
+            ptr->scroll_y = next + 1;
+        }
+    }
+}
+#include "pe1/menu_widget.h"
+
 void MenuWidget_ClampCursor(MenuWidgetNode *ptr, int x, int y) {
     int limit;
     int top;
