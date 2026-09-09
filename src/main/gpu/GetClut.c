7212f6581e893cdf98df169145d4705939e3fff5
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 02:52:10 +0200

    Merge texture identifier translation unit

--- src/main/gpu/GetClut.c
+++
@@ -1,5 +0,0 @@
-typedef unsigned short u_short;
-
-u_short GetClut(int x, int y) {
-    return (y << 6) | ((x >> 4) & 0x3F);
-}
--- src/main/gpu/GetTPage.c
+++ src/main/gpu/GetTPage.c
@@ -4,3 +4,10 @@ u_short GetTPage(int tp, int abr, int x, int y) {
     return (((tp & 0x3) << 7) | ((abr & 0x3) << 5) | ((y & 0x100) >> 4) |
             ((x & 0x3FF) >> 6) | ((y & 0x200) << 2));
 }
+
+static unsigned int GetTPage_alignment __attribute__((section(".text"))) = 0;
+
+
+u_short GetClut(int x, int y) {
+    return (y << 6) | ((x >> 4) & 0x3F);
+}
