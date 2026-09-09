6e2ab97c211d814a32d45c6d8a24644e98bc8438
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 04:42:11 +0200

    Merge memory-card write protocol translation unit

--- src/main/memcard/MemCard_WriteProtocol.c
+++ src/main/memcard/MemCard_WriteProtocol.c
@@ -3,12 +3,35 @@
 
 #include "pe1/card_obj.h"
 
+extern void (*D_8009B750)(void);
+extern void (*D_8009B754)(void);
+extern int D_8009B760;
+extern int D_8009B764;
+extern int g_MemCardPort1Present;
 extern void (*g_MemCardStateDispatchFn)(void *);
-extern int g_MemCardCommandByte;
 extern int g_MemCardDispatchResult;
+extern int g_MemCardCommandByte;
 extern int g_MemCardWriteBlockCount;
 
 int MemCard_WriteByte(CardObj *obj, int value);
+
+void CardObj_WriteCommandByte(CardObj *obj) {
+    if (D_8009B764 == g_MemCardPort1Present && D_8009B760 != 0) {
+        D_8009B754();
+        D_8009B750();
+    }
+
+    if (g_MemCardDispatchResult != 0) {
+        g_MemCardStateDispatchFn(obj->field_0c);
+        g_MemCardStateDispatchFn(obj->field_0c + 0xF0);
+    }
+
+    if (obj->command == 0) {
+        MemCard_WriteByte(obj, 0x42);
+    } else {
+        MemCard_WriteByte(obj, obj->command);
+    }
+}
 
 int MemCard_WriteDataAndGetSize(CardObj *obj) {
     int result;
