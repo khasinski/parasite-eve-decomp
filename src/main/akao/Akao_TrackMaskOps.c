310780ab8b7dbd9b58a16507a9d25bb94a345539
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 09:49:06 +0200

    Merge AKAO track mask operations translation unit

--- src/main/akao/Akao_TrackMaskOps.c
+++ src/main/akao/Akao_TrackMaskOps.c
@@ -36,3 +36,31 @@ void SeqOp_ClearTrack34Mask(AkaoTrack *track, u32 mask) {
     Seq_MarkTrack34MaskDirty();
     track->key_on_delay = 0;
 }
+
+#include "pe1/akao.h"
+
+extern char *g_AkaoCurTrack;
+extern u32 g_AkaoTrack3CMask;
+
+void Seq_MarkTrack3CMaskDirty(void);
+
+void SeqOp_SetMask(AkaoTrack *track, u32 mask) {
+    if (track->parent_track_id == 0) {
+        ((AkaoTrack *)g_AkaoCurTrack)->voice_mask_b |= mask;
+    } else if ((track->flags & 0x10000) != 0) {
+        g_AkaoTrack3CMask |= mask;
+    }
+
+    Seq_MarkTrack3CMaskDirty();
+}
+
+void SeqOp_ClearTrack3CMask(AkaoTrack *track, u32 mask) {
+    if (track->parent_track_id == 0) {
+        ((AkaoTrack *)g_AkaoCurTrack)->voice_mask_b &= ~mask;
+    } else {
+        g_AkaoTrack3CMask &= ~mask;
+    }
+
+    Seq_MarkTrack3CMaskDirty();
+    track->key_off_delay = 0;
+}
