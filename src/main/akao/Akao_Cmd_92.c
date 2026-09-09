6579a99024fad05fbf5d7772522228b8bcd5bb0e
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 09:43:47 +0200

    Merge AKAO command encoder translation unit

--- src/main/akao/Akao_Cmd_92.c
+++
@@ -1,7 +0,0 @@
-#include "pe1/akao/commands.h"
-
-void Akao_Cmd_92(int arg0) {
-    g_AkaoCmdOpcode = 0x92;
-    g_AkaoCmdArg0 = arg0;
-    Akao_EnqueueStagedCommand();
-}
--- src/main/akao/Akao_CommandEncoders.c
+++ src/main/akao/Akao_CommandEncoders.c
@@ -18,3 +18,17 @@ void Akao_Cmd_80_81_82(int arg0) {
     g_AkaoCmdOpcode = opcode;
     Akao_EnqueueStagedCommand();
 }
+#include "pe1/akao/commands.h"
+
+void Akao_Cmd_90(int arg0) {
+    g_AkaoCmdOpcode = 0x90;
+    g_AkaoCmdArg0 = arg0 & 0xFFFFFF;
+    Akao_EnqueueStagedCommand();
+}
+#include "pe1/akao/commands.h"
+
+void Akao_Cmd_92(int arg0) {
+    g_AkaoCmdOpcode = 0x92;
+    g_AkaoCmdArg0 = arg0;
+    Akao_EnqueueStagedCommand();
+}
