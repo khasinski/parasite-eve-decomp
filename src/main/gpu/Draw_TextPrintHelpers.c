4ae38843d5e95db9eb77af1f813a20a5e10062d3
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 06:40:29 +0200

    Merge text print helper translation unit

--- src/main/gpu/Draw_TextPrintHelpers.c
+++ src/main/gpu/Draw_TextPrintHelpers.c
@@ -1,3 +1,14 @@
+/* CC1_FLAGS: -G8 */
+/* MASPSX_FLAGS: -G8 */
+
+extern int g_DrawTextBoxWidth;
+
+void Draw_PrintCenteredTextInWidth(int arg0, int arg1);
+
+void Draw_PrintCenteredText(int arg0) {
+    Draw_PrintCenteredTextInWidth(arg0, g_DrawTextBoxWidth);
+}
+
 #include "common.h"
 /* CC1_FLAGS: -G8 */
 /* MASPSX_FLAGS: -G8 */
