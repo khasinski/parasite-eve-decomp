91747a96b2ae60b7266bf6dc2769209b14f3318d
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 09:32:13 +0200

    Merge AKAO playback bank translation unit

--- src/main/akao/Akao_PlaybackBank.c
+++ src/main/akao/Akao_PlaybackBank.c
@@ -1,6 +1,26 @@
 #include "common.h"
 #include "pe1/akao.h"
 
+extern u16 g_AkaoSelectedBankId;
+extern char *g_AkaoCurTrack;
+
+void Akao_UpdateVoiceMask(int arg0);
+void Akao_StepSequencerVoice(void *arg0);
+
+void Seq_SelectPlaybackBank(int *arg0) {
+    u16 value;
+
+    value = g_AkaoSelectedBankId;
+    if ((value != 0) && (value == arg0[3])) {
+        Akao_UpdateVoiceMask(arg0[1]);
+    } else {
+        Akao_StepSequencerVoice(arg0[1]);
+        ((AkaoTrack *)g_AkaoCurTrack)->parent_track_id = arg0[3];
+    }
+}
+#include "common.h"
+#include "pe1/akao.h"
+
 extern char g_AkaoTrackStateBackup[];
 extern AkaoVoiceBank g_AkaoVoiceStateBackup[];
 extern AkaoVoiceBank g_AkaoVoiceBanks[] __asm__("g_AkaoVoiceStateTable");
