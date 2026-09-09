4e70083d5e5d70042999926ed80dd4cc7b6dae99
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 09:59:07 +0200

    Merge entity action animation translation unit

--- src/main/task/Task_EntityActionAnimation.c
+++ src/main/task/Task_EntityActionAnimation.c
@@ -13,3 +13,20 @@ int Entity_SetCurrentActionMode(u16 **arg0) {
     entity->flags &= ~0x100;
     return 1;
 }
+#include "common.h"
+#include "pe1/field_actor.h"
+
+
+
+int Task_SetEntityAnimFrame(u16 **arg0) {
+    FieldActor *entity = g_CurrentEntity;
+    u16 value = *arg0[0];
+
+    if (entity->action < value) {
+        value = entity->action;
+    }
+
+    entity->anim_frame_target = value;
+    entity->flags |= 0x200;
+    return 1;
+}
