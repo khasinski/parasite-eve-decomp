e3a149867f0d29d58035be6ab36a1d93ce38b99e
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 06:25:35 +0200

    Merge equip list adapter translation unit

--- src/main/menu/Menu_DrawArmorList.c
+++
@@ -1,8 +0,0 @@
-void Menu_DrawArmorItemRow(void);
-void Inv_SelectActiveList(int useOverride);
-void MenuWidget_DrawList(int arg0, void (*callback)(void));
-
-void Menu_DrawArmorList(int arg0) {
-    Inv_SelectActiveList(1);
-    MenuWidget_DrawList(arg0, Menu_DrawArmorItemRow);
-}
--- src/main/menu/Menu_DrawWeaponList.c
+++ src/main/menu/Menu_DrawWeaponList.c
@@ -6,3 +6,12 @@ void Menu_DrawWeaponList(int arg0) {
     Inv_SelectActiveList(0);
     MenuWidget_DrawList(arg0, Menu_DrawInventoryItemRow);
 }
+
+void Menu_DrawArmorItemRow(void);
+void Inv_SelectActiveList(int useOverride);
+void MenuWidget_DrawList(int arg0, void (*callback)(void));
+
+void Menu_DrawArmorList(int arg0) {
+    Inv_SelectActiveList(1);
+    MenuWidget_DrawList(arg0, Menu_DrawArmorItemRow);
+}
