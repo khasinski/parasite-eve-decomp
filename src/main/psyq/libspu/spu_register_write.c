498a27bd33f496ae45abb5bb39a34d50bb56d411
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 02:11:06 +0200

    Merge Psy-Q SPU register translation unit

--- src/main/psyq/libspu/spu_register_write.c
+++ src/main/psyq/libspu/spu_register_write.c
@@ -62,3 +62,55 @@ u32 _spu_FsetRXXa(s32 arg0, u32 value) {
     }
     }
 }
+
+u32 _spu_FgetRXXa(u32 offset, s32 mode)
+{
+  register u32 value;
+  value = *((u16 *) ((unsigned char *)_spu_RXX + (offset * 2)));
+  if (mode == (-1))
+  {
+    return *((u16 *) ((unsigned char *)_spu_RXX + (offset * 2)));
+  }
+  return (*((u16 *) ((unsigned char *)_spu_RXX + (offset * 2)))) << _spu_mem_mode_plus;
+}
+
+#include "pe1/psyq_spu_internal.h"
+
+void _spu_FsetPCR(s32 high_priority) {
+    *_spu_sys_pcr &= 0xFFF8FFFF;
+
+    if (high_priority) {
+        *_spu_sys_pcr |= 0x30000;
+    } else {
+        *_spu_sys_pcr |= 0x50000;
+    }
+}
+
+void _spu_FsetDelayW(void) {
+    *_spu_delay = (*_spu_delay & 0xF0FFFFFF) | 0x20000000;
+}
+
+void _spu_FsetDelayR(void) {
+    *_spu_delay = (*_spu_delay & 0xF0FFFFFF) | 0x22000000;
+}
+
+
+typedef struct SpuWaitWork {
+    volatile int i;
+    volatile int value;
+} SpuWaitWork;
+
+void _spu_Fw1ts(void) {
+    register SpuWaitWork *work asm("$29");
+    int current;
+
+    work--;
+    work->value = 0xD;
+    *(int *)&work->i = 0;
+    while (work->i < 0x3C) {
+        current = work->value;
+        work->value = (((current << 1) + current) << 2) + current;
+        work->i = work->i + 1;
+    }
+    work++;
+}
