ef6e0a487bedb3ed3654f5cdd456d2faeba80040
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 05:50:19 +0200

    Type task node flag handler

--- src/main/task/Task_SetNodeFlag80.c
+++ src/main/task/Task_SetNodeFlag80.c
@@ -1,9 +1,11 @@
 /* CC1_FLAGS: -G8 */
 /* MASPSX_FLAGS: -G8 */
 
-extern char *g_TaskNodePool;
+#include "pe1/task_node.h"
+
+extern TaskNode *g_TaskNodePool;
 
 int Task_SetNodeFlag80(void) {
-    *(unsigned short *)(g_TaskNodePool + 8) |= 0x80;
+    g_TaskNodePool->flags |= 0x80;
     return 1;
 }
