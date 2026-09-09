a755bd0a5faae9e5aa9720254230b07041227c6f
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 05:18:28 +0200

    Merge menu widget pool translation unit

--- src/main/menu/MenuWidget_Pool.c
+++ src/main/menu/MenuWidget_Pool.c
@@ -1,11 +1,30 @@
-#include "common.h"
-#include "pe1/menu_widget.h"
 /* CC1_FLAGS: -G8 */
 /* MASPSX_FLAGS: -G8 */
 
-#define NULL ((void *)0)
-
+#include "common.h"
+#include "pe1/menu_widget.h"
 #include "../../../tools/m2c/m2c_macros.h"
+
+void MenuWidget_InitPoolUnk(void) {
+    MenuWidgetNode *node;
+    MenuWidgetNode *end;
+    MenuWidgetNode *next;
+
+    node = (MenuWidgetNode *)g_MenuWidgetNodePool;
+    end = node + 24;
+
+    while (node < end) {
+        next = node + 1;
+        node->next = next;
+        node = next;
+    }
+
+    g_MenuWidgetNodePoolSentinel.next = 0;
+    g_MenuWidgetFreeListHead = (MenuWidgetNode *)&g_MenuWidgetNodePoolSentinel - 23;
+    g_MenuWidgetCurrentNode = 0;
+    g_MenuWidgetActiveListHead = 0;
+}
+#define NULL ((void *)0)
 
 M2C_UNK BoundsCheck_AssertStub();
 
