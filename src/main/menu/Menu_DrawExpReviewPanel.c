224fabe9066bdffc1f74555522eae1f54a6b2435
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-09 06:16:06 +0200

    Merge experience review translation unit

--- src/main/menu/Menu_DrawExpReviewPanel.c
+++ src/main/menu/Menu_DrawExpReviewPanel.c
@@ -1,13 +1,14 @@
 #include "common.h"
 #include "pe1/menu_state.h"
+#include "pe1/menu_widget.h"
 /* CC1_FLAGS: -G8 */
 /* MASPSX_FLAGS: -G8 */
 
 void Akao_FlushBgmVolumeFade(void);
 void *Aya_GetLevelExpTable(void);
 s32 Stat_BinarySearch(s32 value, void *table);
-void *MenuWidget_FindByModeAndSelectedBase(s32 mode, s32 selected_base);
-void MenuWidget_DestroyNode(void *node);
+MenuWidgetNode *MenuWidget_FindByModeAndSelectedBase(s32 mode, s32 selected_base);
+void MenuWidget_DestroyNode();
 void Menu_CreateLevelUpBanner(void);
 void Draw_OffsetCursor(s32 x, s32 y);
 void Draw_StatePush(void);
@@ -77,4 +78,54 @@ void Menu_DrawExpReviewPanel(void) {
     Draw_OffsetCursor(0x74, 0);
     Draw_PrintNumberWidth2(Menu_GetBattleCount());
     Draw_StatePop();
+}
+
+#include "common.h"
+/* CC1_FLAGS: -G8 */
+/* MASPSX_FLAGS: -G8 */
+
+#include "include_asm.h"
+
+void MenuWidget_DestroyNode();
+void Menu_StepSaveLoadScreen(void);
+void Menu_CreateLevelUpResultPanel(void);
+void Inv_SetActiveList(s32 arg0, s32 arg1);
+void Menu_PlayConfirmSound(void);
+void Akao_SetBgmVolumeFade(void);
+void Akao_FlushBgmVolumeFade(void);
+s32 Menu_GetBattleCount(void);
+s32 Stat_BinarySearch(s32 value, void *table);
+void *Aya_GetLevelExpTable(void);
+
+extern s32 g_MenuPendingTotalExp;
+extern s32 g_MenuExpAllocTarget;
+extern s32 g_AyaSaveTotalExp[];
+#define g_AyaSaveTotalExp (g_AyaSaveTotalExp[0])
+
+s32 Menu_ConfirmExpAllocation(s32 unused, s32 flags) {
+    s32 confirm;
+    s32 new_level;
+
+    confirm = flags & 0x10000;
+    if (confirm != 0) {
+        if (g_MenuPendingTotalExp < g_MenuExpAllocTarget) {
+            g_MenuPendingTotalExp = g_MenuExpAllocTarget;
+        } else {
+            MenuWidget_DestroyNode();
+            MenuWidget_DestroyNode(MenuWidget_FindByModeAndSelectedBase(1, 0x16));
+            Akao_FlushBgmVolumeFade();
+            new_level = Stat_BinarySearch(g_MenuPendingTotalExp, Aya_GetLevelExpTable());
+            if (new_level != Stat_BinarySearch(g_AyaSaveTotalExp, Aya_GetLevelExpTable())) {
+                Menu_CreateLevelUpResultPanel();
+                Akao_SetBgmVolumeFade();
+            } else if (Menu_GetBattleCount() != 0) {
+                Menu_StepSaveLoadScreen();
+            } else {
+                Inv_SetActiveList(0xA, 0);
+            }
+            g_AyaSaveTotalExp = g_MenuPendingTotalExp;
+        }
+        Menu_PlayConfirmSound();
+    }
+    return 1;
 }
