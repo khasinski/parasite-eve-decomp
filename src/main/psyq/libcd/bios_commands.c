a05c9f104fcf113a6d415dce3bfdab0859157e08
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-10 08:59:24 +0200

    Merge adjacent LIBCD BIOS routines

--- src/main/psyq/libcd/bios_commands.c
+++ src/main/psyq/libcd/bios_commands.c
@@ -1,4 +1,18 @@
+/* GCC_VERSION: 2.8.1 */
+/* CC1_FLAGS: -mno-split-addresses -fno-expensive-optimizations */
+
 #include "pe1/psyq_cd.h"
+
+int CD_vol(CdlATV *vol) {
+    *g_CdRegIndexBase = 2;
+    *g_CdRegDataWrite = vol->val0;
+    *g_CdRegResponse = vol->val1;
+    *g_CdRegIndexBase = 3;
+    *g_CdRegPort1 = vol->val2;
+    *g_CdRegDataWrite = vol->val3;
+    *g_CdRegResponse = 0x20;
+    return 0;
+}
 
 void CD_flush(void) {
     register CdInterruptEvents *state asm("$3");
