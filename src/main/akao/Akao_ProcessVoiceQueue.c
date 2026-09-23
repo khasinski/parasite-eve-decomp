0dcbda6baff992eb03671ab797b7a87adeefe17a
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-24 00:30:35 +0200

    Decompile Akao_ProcessVoiceQueue with exact retail match

---
+++ src/main/akao/Akao_ProcessVoiceQueue.c
@@ -0,0 +1,130 @@
+#include "pe1/akao/voice_masks.h"
+extern unsigned D_800BCD58, g_AkaoVoiceMaskScratch;
+extern unsigned short g_AkaoTrack5ATransposeValue;
+extern AkaoTrack g_AkaoVoiceChannelTable[];
+void Akao_UpdateVoiceEnvelopes(unsigned);
+void Akao_StepVoiceNote(AkaoTrack *, unsigned, unsigned, unsigned *);
+void Akao_SetVoiceKeyOn(AkaoTrack *, unsigned);
+void Akao_WriteVoiceParam(int, int *, unsigned);
+void Akao_SetMasterVolume(short, short);
+long SpuSetNoiseClock(long);
+void Spu_WriteReverbEnable(unsigned);
+void Spu_WriteNoiseEnable(unsigned);
+void Spu_WriteFmEnable(unsigned);
+void Spu_WriteKeyOn(unsigned);
+void Akao_ProcessVoiceQueue(void)
+{
+  unsigned *active = &g_SpuActiveVoiceMask;
+  unsigned excluded;
+  unsigned secondary;
+  unsigned secondary_new;
+  unsigned int allocated_primary;
+  unsigned primary;
+  unsigned primary_new;
+  unsigned result = 0;
+
+  register unsigned bit asm("$18");
+  unsigned flags;
+  AkaoSequencerBank *bank;
+  AkaoTrack *track;
+  excluded = (*active) | g_SpuStoppedVoiceMask;
+  if ((g_AkaoCurTrack[0].active_voice_mask & g_AkaoCurTrack[0].key_on_request_mask) | (g_AkaoCurTrack[1].active_voice_mask & g_AkaoCurTrack[1].key_on_request_mask))
+  {
+    Akao_UpdateVoiceEnvelopes(excluded);
+  }
+  secondary = (g_AkaoCurTrack[1].active_voice_mask & g_AkaoCurTrack[1].allocated_voice_mask) & (~(g_AkaoCurTrack[1].field_0C & excluded));
+  secondary_new = (secondary & g_AkaoCurTrack[1].field_0C) & (~excluded);
+  if (secondary & g_AkaoCurTrack[1].pending_voice_mask)
+  {
+    g_AkaoCurTrack++;
+    Akao_StepVoiceNote(g_AkaoVoiceStateTable2, secondary & g_AkaoCurTrack->pending_voice_mask, secondary_new, &result);
+    bank = g_AkaoCurTrack;
+    secondary &= ~bank->pending_voice_mask;
+    g_AkaoCurTrack--;
+    bank->key_on_request_mask &= ~bank->pending_voice_mask;
+  }
+  allocated_primary = g_AkaoCurTrack->active_voice_mask & g_AkaoCurTrack->allocated_voice_mask;
+  primary_new = secondary_new | excluded;
+  primary = allocated_primary & (~(g_AkaoCurTrack->field_0C & primary_new));
+  excluded = (primary & g_AkaoCurTrack->field_0C) & (~primary_new);
+  if (primary & g_AkaoCurTrack->pending_voice_mask)
+  {
+    Akao_StepVoiceNote(g_AkaoVoiceStateTable, primary & g_AkaoCurTrack->pending_voice_mask, excluded, &result);
+    bank = g_AkaoCurTrack;
+    primary &= ~bank->pending_voice_mask;
+    asm("" : : : "memory");
+    bank->key_on_request_mask &= ~bank->pending_voice_mask;
+  }
+  asm("" : : "r"(secondary));
+  if (secondary)
+  {
+    g_AkaoCurTrack++;
+    Akao_StepVoiceNote(g_AkaoVoiceStateTable2, secondary, secondary_new & (~excluded), &result);
+    g_AkaoCurTrack->key_on_request_mask = 0;
+    g_AkaoCurTrack--;
+  }
+  if (primary)
+  {
+    Akao_StepVoiceNote(g_AkaoVoiceStateTable, primary, excluded, &result);
+    g_AkaoCurTrack->key_on_request_mask = 0;
+  }
+  primary = (*active) & D_800BCD58;
+  if (primary)
+  {
+    bit = 0x1000;
+    track = g_AkaoVoiceChannelTable;
+    result |= g_AkaoVoiceMaskScratch;
+    while (primary)
+    {
+      if (primary & bit)
+      {
+        Akao_SetVoiceKeyOn(track, bit);
+        if (track->update_flags)
+        {
+          Akao_WriteVoiceParam(track->assigned_voice_index, &track->assigned_voice_index, track->flags);
+        }
+        secondary_new = bit;
+        primary &= ~secondary_new;
+      }
+      bit <<= 1;
+      track++;
+    }
+
+    g_AkaoVoiceMaskScratch = 0;
+  }
+  primary = g_AkaoVoiceUpdateFlags;
+  if (primary & 0x80)
+  {
+    short volume = ((short *) g_AkaoCurTrack->field_40)[1];
+    Akao_SetMasterVolume(volume, volume);
+    g_AkaoVoiceUpdateFlags &= ~0x80;
+  }
+  if (primary & 0x10)
+  {
+    unsigned short clock;
+    if (g_SpuActiveVoiceMask)
+    {
+      clock = g_AkaoTrack5ATransposeValue;
+    }
+    else
+    {
+      clock = g_AkaoCurTrack->field_5A;
+    }
+    SpuSetNoiseClock(clock);
+    g_AkaoVoiceUpdateFlags &= ~0x10;
+  }
+  if (0x100 & primary)
+  {
+    Akao_SetVoiceAdsr();
+    Akao_SetVoiceVolume();
+    Akao_SetVoiceStartAddr();
+    Spu_WriteReverbEnable(D_800C0DD0);
+    Spu_WriteNoiseEnable(D_800C0DD4);
+    Spu_WriteFmEnable(D_800C0DD8);
+    g_AkaoVoiceUpdateFlags &= ~0x100;
+  }
+  if (result)
+  {
+    Spu_WriteKeyOn(result);
+  }
+}
