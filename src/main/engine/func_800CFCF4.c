511a72ffd92cf74f86af48eed83008f9875180f0
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-23 14:38:48 +0200

    Match FieldEng angle interpolation routine

--- src/main/engine/func_800CFCF4.c
+++ src/main/engine/func_800CFCF4.c
@@ -1,5 +1,5 @@
+#include "common.h"
 
-typedef signed short s16;
 int func_800CFCF4(int from, int to, s16 *distance)
 {
   unsigned char new_var;
@@ -32,4 +32,53 @@ int func_800CFCF4(int from, int to, s16 *distance)
     *distance = delta;
   }
   return (s16) direction;
+}
+
+/* Return the shortest turn direction and write its unsigned angle distance. */
+static inline int angle_direction(u16 *fromPtr, u16 *toPtr, s16 *distance) {
+    int from = *fromPtr & 0xFFF;
+    int to = *toPtr & 0xFFF;
+    u8 negative;
+    int direction;
+    int delta;
+    int direction16;
+    int diff;
+
+    direction = -1;
+    if ((unsigned int)to < (unsigned int)from) direction = 1;
+    diff = from - to;
+    negative = diff < 0;
+    delta = diff;
+    if (negative) delta = -delta;
+    if (delta >= 0x801) {
+        direction16 = (s16)direction;
+        direction = -direction16;
+        delta = 0x1000 - delta;
+    }
+    *distance = delta;
+    return (s16)direction;
+}
+
+void func_800CFD50(u16 *from, u16 *to, u16 speed) {
+    s16 distance;
+    int direction;
+    int scaled;
+    u16 old0, old1;
+    s16 change;
+
+    direction = angle_direction(&from[0], &to[0], &distance);
+    scaled = distance * speed;
+    if (scaled < 0) scaled += 0xFFF;
+    old0 = to[0];
+    distance = scaled >> 12;
+    change = distance * direction;
+    to[0] = old0 + change;
+
+    direction = angle_direction(&from[1], &to[1], &distance);
+    scaled = distance * speed;
+    if (scaled < 0) scaled += 0xFFF;
+    old1 = to[1];
+    distance = scaled >> 12;
+    change = distance * direction;
+    to[1] = old1 + change;
 }
