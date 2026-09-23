feabf0d6ca8b0c24bcb85d59f8617a13c3ce8a8f
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-23 06:41:42 +0200

    Decompile room m152 handler with shared return tail

---
+++ src/overlays/room_m152/RoomLib_HandlerDArgs_8018F2C0.c
@@ -0,0 +1,7 @@
+#include "../room_lib/room_lib.h"
+
+void RoomLib_NotifyArmB_8018F5D4(RoomEnt *o);
+
+/* The final return is also the retail entry point at 0x8018F53C. */
+ROOMLIB_HANDLER_D_ARGS(RoomLib_HandlerDArgs_8018F2C0,
+                       RoomLib_NotifyArmB_8018F5D4)
