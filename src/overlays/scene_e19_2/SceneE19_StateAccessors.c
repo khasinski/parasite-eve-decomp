abb6191f723da44dd7497ad109cdc08c8f731b05
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-25 21:45:55 +0200

    Match scene E19 state accessors

---
+++ src/overlays/scene_e19_2/SceneE19_StateAccessors.c
@@ -0,0 +1,33 @@
+#include "scene_e19_state.h"
+
+u32 *func_801951E0(void) {
+    return &D_8019B668;
+}
+
+s16 *func_801951F0(int unused, s16 first, s16 second) {
+    s16 *result = &D_8019B66C;
+
+    *result = first;
+    D_8019B66E = second;
+    return result;
+}
+
+u32 *func_8019520C(void) {
+    return &D_8019B668;
+}
+
+u32 *func_8019521C(void) {
+    return &D_8019B668;
+}
+
+u32 *func_8019522C(void) {
+    return &D_8019B668;
+}
+
+u32 *func_8019523C(void) {
+    return &D_8019B668;
+}
+
+u32 *func_8019524C(void) {
+    return &D_8019B668;
+}
---
+++ src/overlays/scene_e19_2/scene_e19_state.h
@@ -0,0 +1,10 @@
+#ifndef SCENE_E19_STATE_H
+#define SCENE_E19_STATE_H
+
+#include "common.h"
+
+extern u32 D_8019B668;
+extern s16 D_8019B66C;
+extern s16 D_8019B66E;
+
+#endif
