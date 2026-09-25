a545cce6e35d8626d52f53171f02809cc760d1fa
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-09-25 23:29:44 +0200

    Decompile shared scene draw motion with exact overlay bytes

---
+++ src/overlays/scene_e09/RoomLib_DrawMotion_80190454.c
@@ -0,0 +1,6 @@
+/* CC1_FLAGS: -fno-strength-reduce */
+#define func_80190410 func_80190454
+#define D_801940C0 D_80194E10
+#define D_801940C2 D_80194E12
+#define D_80194008 D_80194CF0
+#include "../room_lib/RoomLib_DrawMotion.inc"
